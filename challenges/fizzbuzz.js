44b0595a99af8b5086681a21d7881c05f0c31e5f
scottrosen14 <scott.rosen14@gmail.com>
2017-09-13 13:52:55 -0700

    recursion not working

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,27 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  // input is a number
+  // output an array
+  // if num % 3 === 0 buzz, num % 5 === 0 fizz, both fizzbuzz
+  // see if we can print
+  var array = [];
+  // base case
+  if (num === 0) {
+    return array;
+  }
+  if (num % 3 === 0) {
+    array.unshift('fizz');
+  } else if (num % 5 === 0) {
+    array.unshift('buzz');
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    array.unshift('fizzbuzz');
+  } else {
+    array.unshift(num);
+  }
+  // calls recursively
+  // returns array
+  return fizzbuzz(num - 1);
 }
-
+console.log(fizzbuzz(10))
 module.exports = fizzbuzz;
