b7c66374392668a31d3a369384a74d104d1f415b
louis <louisrouaze@gmail.com>
2017-09-13 09:55:38 -0700

    fizzbuzz done

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,28 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+    let arr = []
+    if (num === undefined) return arr;
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            arr.push('fizzbuzz');
+        } else if (i % 3 === 0) {
+            arr.push('fizz');
+        } else if (i % 5 === 0) {
+            arr.push('buzz');
+        } else {
+            arr.push(i);
+        }
 
+    }
+    return arr;
 }
 
 module.exports = fizzbuzz;
+
+//return array
+//iterate throught every numbers(i)
+// if i % 3 and 5 = 0 return fizzbuzz
+// if i % 3 = 0 return fizz
+// if i % 5 = 0 return buzz
+
