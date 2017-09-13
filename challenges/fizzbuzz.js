56966d2d80a5e68653e94cad1acde1b42bdcc6ed
Pamela Lam <pamela.le.lam@gmail.com>
2017-09-13 09:56:41 -0700

    completed fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,19 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  let results = [];
+  for(let i = 1; i <= num; i++) {
+    if((i%3 === 0) && (i%5 === 0)) {
+      results.push("fizzbuzz");
+    } else if (i%3 === 0) {
+      results.push("fizz");
+    } else if (i%5 === 0) {
+      results.push("buzz");
+    } else {
+      results.push(i);
+    }
+  }
+  return results;
 }
 
 module.exports = fizzbuzz;
