6cb070dae03e2f10462296ddcb6891f94cfc27cb
Vince Vuong <vuong.vincent@gmail.com>
2017-09-15 09:51:39 -0700

    Completed Stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,18 @@
 */
 
 function drawStairs(n) {
-
+// create result variable
+let stairs = "";
+// create a for loop to iterate through number of stairs 
+  for (let i = 1; i <= n; i++) {
+    //create number of spaces(n - i) concat with stair (n)
+    stairs += ' '.repeat(n-i) + '*'.repeat(i) + '\n';
+  }
+// return result;
+return stairs;
 }
+
+console.log(drawStairs(6));
 
 
 module.exports = drawStairs;
