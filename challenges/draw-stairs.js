3a8f5bb12b921a4ac65d922d27328dcf399a06ed
scottrosen14 <scott.rosen14@gmail.com>
2017-09-16 10:10:44 -0700

    completed drawStairs using nested for loops

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
  The staircase must climb up from left to right. The last line should only consist of asterisks,
  without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+
+ For example:
+ drawStairs(6) ->
      *
     **
    ***
@@ -14,8 +14,25 @@
 */
 
 function drawStairs(n) {
-
+  // n btwn 1 and 100
+  // aster length always === n
+  // iterate and concat space until length - i
+  // else concat *
+  // print the aster
+  let stair = '';
+  let space = ' ';
+  let aster = '*';
+    for (let i = 1; i <= n; i++) {
+      stair = '';
+      for (let g = 1; g <= n-i; g++) {
+        stair += space;
+      }
+      for (let g = 1; g <= i; g++) {
+        stair += aster;
+      }
+      console.log(stair)
+    }
 }
-
+drawStairs(5)
 
 module.exports = drawStairs;
