be312297da009258b4387b70eeaff2d82650f2a4
hcam05 <harry@simpleplots.com>
2017-09-16 09:55:01 -0700

    mode/mean challenge submission

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
-
+    
+    //mean 
+    let sum = array.reduce((x,y) => x + y);
+    let length = array.length
+    let mean = Math.floor(sum/length);
+    
+    //Mode//
+    //build table to tally values
+    let tally = array.reduce((x,y) => {
+        if(y in x){
+            x[y] += 1
+        }else{
+            x[y] = 1
+        }
+        return x;
+    },{});
+  
+   //find the high number of tally object
+   let highNum =  Math.max(...Object.values(tally));
+   
+   //get the highest number from tally object
+   let getVal = (obj, val) => Object.keys(obj).find(key => obj[key] === val);
+  
+   let highTally = Number(getVal(tally, highNum));
+ 
+   return mean == highTally;
 }
 
 module.exports = modemean;
