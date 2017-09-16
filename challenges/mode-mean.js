0b0be71b19216900a1b086a9d09c6d976c4f2ea0
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-09-16 09:28:23 -0700

    Complete mode-mean.js

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
+    let sum = 0;
+    let mean;
+    let obj = {};
 
+    for (element of array) {
+        sum += element;
+        if (obj[element]) obj[element] += 1;
+        else obj[element] = 1;
+    }
+    
+    mean = sum / array.length;
+    
+    let modeNum = 0;
+    let modeKey;
+
+    for (key in obj) {
+        if (modeNum < obj[key]) {
+            modeNum = obj[key];
+            modeKey = Number(key);
+        }
+    }
+
+    return mean === modeKey;
 }
 
 module.exports = modemean;
