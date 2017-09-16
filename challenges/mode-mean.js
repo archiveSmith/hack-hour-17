7ad8a7c8ea93a34461c66c4a138ad82c3e3f82e1
kenhughlee <kenhughlee@yahoo.com>
2017-09-16 09:41:50 -0700

    complete modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,28 @@
 
 
 function modemean(array) {
+  let modeObj = {};
+  let mean = return array.reduce(function(acc,curr) {
+    return acc + curr;
+  },0) / array.length;
 
+``
+array.forEach(function(element)  {
+  if  (element in modeObj)
+    modeObj[element] ++;
+    else;
+  modeObj[element] = 1;
+});
+let findHigh, prev = [0,0];
+for (let i in modeObj) {
+  if (prev[1] <= modeObj[i]) {
+    prev[0] = i;
+    prev[1] = modeObj[i];
+  }
 }
 
+if (prev[0] === mean)
+  return true;
+  else return false;
+}
 module.exports = modemean;
