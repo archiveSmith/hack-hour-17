08fd1ae2bda9b4200c50d6470cc0d4ac0d5a069e
Jo Zhou <thejozhou@gmail.com>
2017-09-18 11:17:00 -0700

    Mode Mean Solution

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,32 @@
 
 
 function modemean(array) {
+  const sum = array.reduce((a, b) => a + b, 0);
+  const mean = Math.floor(sum / array.length);
 
+  const modeCollector = {};
+  let maxCount = 1;
+  let maxNum = array[0];
+
+  for (let i = 0; i < array.length; i += 1) {
+
+    const num = array[i];
+
+    if (modeCollector[num] === undefined) {
+      modeCollector[num] = 1;
+    } else {
+      modeCollector[num] += 1;
+    }
+
+    if (modeCollector[num] > maxCount) {
+      maxNum = num;
+      maxCount = modeCollector[num];
+    }
+  }
+  console.log('mean ', mean, 'mode ', maxNum)
+  return mean === maxNum;
 }
+
+console.log(modemean([1, 3, 3, 10, 3, 4]));
 
 module.exports = modemean;
