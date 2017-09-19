bb199d9fc2b589a481485410e7c9cbdbe552c8c0
woojunchoi <emilyhongji@Emily-Kim.local>
2017-09-19 09:46:36 -0700

    finished

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,7 +21,34 @@ function Node(val) {
   this.next = null;
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 function kthToLastNode(k, head) {
+  if(head === undefined) {
+    head = a;
+  }
+  if(k === 0) {
+    return head;
+  }
+  let counter = 0;
+  let tail;
+  let obj = {}
+
+  while(head) {
+    obj[counter] = head
+    head = head.next
+    counter++
+  
+    }
+
+
 
 }
 
