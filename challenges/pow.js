790b082f25c3c3a6e56af302af3b2ce70494eab7
Vu Phung <vphung5707@gmail.com>
2017-09-20 09:58:55 -0700

    comment console log test case.

--- challenges/pow.js
+++ challenges/pow.js
@@ -22,11 +22,11 @@ function pow(base, power) {
 // console.log(pow(-2, -2));
 
 
- console.log(pow(2, 0));
- console.log(pow(2, -1));
- console.log(pow(2, -2));
- console.log(pow(2, -3));
- console.log(pow(2, -4));
- console.log(pow(-2, -1));
- console.log(pow(2, -2));
+ // console.log(pow(2, 0));
+ // console.log(pow(2, -1));
+ // console.log(pow(2, -2));
+ // console.log(pow(2, -3));
+ // console.log(pow(2, -4));
+ // console.log(pow(-2, -1));
+ // console.log(pow(2, -2));
 module.exports = pow;
