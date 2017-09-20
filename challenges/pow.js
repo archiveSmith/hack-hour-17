d0256b74256cecf3a65220cb8e92a87548b90b4f
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-09-20 09:27:57 -0700

    Clean up code

--- challenges/pow.js
+++ challenges/pow.js
@@ -5,9 +5,7 @@
 function pow(base, power) {
     if (power < 0) return;
     if (power === 0) return 1;
-    if (power <= 1) {
-        return base;
-    }
+    if (power === 1) return base;
     return base * pow(base, power - 1);
 }
 
