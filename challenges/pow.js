926a150e116a9c7903868ef23580d0e9d69b9bca
szmoon <szmoon@gmail.com>
2017-09-20 09:39:25 -0700

    complete hack hour pow (with recursion!!)

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,6 +4,22 @@
 
 function pow(base, power) {
 
+let total = base;
+
+function calcPow(base,power,total) {
+    // base case: when power is down to 1, return total
+    if (power === 1) return total;
+    // while power is greater than 1, multiply running total by base
+    else if (power > 1) {
+        total = total * base;
+    }
+    // continue to call calcPow and mutiply total by base while power is greater than 1
+    return (calcPow(base, power-1,total));
 }
 
-module.exports = pow;
+return (calcPow(base, power, total));
+
+}
+
+// module.exports = pow;
+console.log(pow(4,5));
