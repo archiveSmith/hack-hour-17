ed5d47ee784bd4cdbf05268b51528e0dfc5c5eb7
Vu Phung <vphung5707@gmail.com>
2017-09-20 09:27:22 -0700

    finish pow problem.

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,20 @@
  */
 
 function pow(base, power) {
+  // if power is negative -1, power NOT number, or base NOT number return -1
+  if (power < 0 || !Number.isInteger(power) || !Number.isInteger(base)) return -1;
+  // if power is 0, return base;
+  if (power === 0) return base;
 
+  return base * pow(base, power - 1);
 }
 
+
+console.log(pow(2, 0));
+console.log(pow(2, 1));
+console.log(pow(2, 2));
+console.log(pow(2, 3));
+console.log(pow(2, 4));
+console.log(pow(2, -1));
+console.log(pow(-2, 0));
 module.exports = pow;
