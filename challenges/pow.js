8742b9b208efd656c648e210af322c906c3b49d7
Samantha Salley <samanthasalley@gmail.com>
2017-09-20 09:39:37 -0700

    complete pow - working with edge cases

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,30 @@
  */
 
 function pow(base, power) {
-
+	if(isNaN(base) || isNaN(power) || (base === 0 && power === 0)) return;
+	if(base === 0) return 0;
+	if(base === 1 || power === 0) return 1;
+	if(power === 1) return base;
+	let result = base;
+	for(let i = 1; i < power; i++){
+		result *= base;
+	}
+	return result;
 }
+
+// ==========
+// TEST CASES
+// ==========
+// console.log('(27) ==> ',pow(3,3));
+// console.log('(9) ==> ',pow(3,2));
+// console.log('(16) ==> ',pow(4,2));
+// console.log('(64) ==> ',pow(2,6));
+// ===== EDGE CASES
+// console.log('(3) ==> ',pow(3,1));
+// console.log('(undefined) ==> ',pow('a',1));
+// console.log('(undefined) ==> ',pow(1,'a'));
+// console.log('(1) ==> ',pow(1,10));
+// console.log('(1) ==> ',pow(10,0));
+// console.log('(0) ==> ',pow(0,10));
 
 module.exports = pow;
