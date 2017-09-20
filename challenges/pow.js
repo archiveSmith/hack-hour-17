7b1bbed5e8da7a869acc44591204c8ef52334463
Vince Vuong <vuong.vincent@gmail.com>
2017-09-20 09:55:25 -0700

    Complete HH

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,11 @@
  */
 
 function pow(base, power) {
-
+  //basecase: power = 0;
+  if (power === 0) return 1;
+  //multiple the base by the result & decrement power
+  return base * pow(base, power - 1);
 }
 
 module.exports = pow;
+
