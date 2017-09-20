2c2cde0598f3963653ab2fa90a457a08d40173cc
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-09-20 10:02:58 -0700

    Power Solution #2

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,9 @@
  */
 
 function pow(base, power) {
-  if (power <= 1) return base;
+  if (power === 1) return base;
+  if (power === 0) return 1;
+  if (power < 0) return 1 / base * pow(base, power + 1);
   return base * pow(base, power - 1);
 }
 
