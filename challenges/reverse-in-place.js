acb5365464d562c5196bddeedcf360afdda9a48a
woojunchoi <emilyhongji@Emily-Kim.local>
2017-09-22 09:47:50 -0700

    finished reverseinplace

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,11 @@
  */
 
 function reverseInPlace(array) {
-
+    var arr = [];
+    for(var i=array.length-1; i>=0; i--) {
+        arr.push(array[i]);
+    }
+    return arr;
 }
 
 module.exports = reverseInPlace;
