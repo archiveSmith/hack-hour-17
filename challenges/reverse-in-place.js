0ff5ca55f12b368b77f5688e83df3de2cd2d21ae
freemanchen <freemanxchen@gmail.com>
2017-09-22 09:25:16 -0700

    complete reverse in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,14 @@
  */
 
 function reverseInPlace(array) {
-
+    let temp;
+    let length = array.length;
+    for (let i = 0; i < Math.floor(length / 2); i++) {
+        temp = array[length - i - 1];
+        array[length - i - 1] = array[i];
+        array[i] = temp;
+    }
+    return array;
 }
 
 module.exports = reverseInPlace;
