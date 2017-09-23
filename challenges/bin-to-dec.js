4bfc4649db2c83991d3ff1b67fb39d558c189dd6
scottrosen14 <scott.rosen14@gmail.com>
2017-09-23 10:07:58 -0700

    completed binary to decimal

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,25 @@
  */
 
 function binToDec(binary) {
-
+  // input a string of ones and zeros
+  // output a number in decimal form
+  // iterate and push powers of 2 to new array
+  // iterate through binary
+    // if 1, then sum = sum + power of 2 [i]
+  let powersOf2 = [];
+  let sum = 0;
+  for (let i = 0; i < binary.length; i++) {
+    powersOf2.unshift(Math.pow(2, i));
+  }
+  for (let r = 0; r < binary.length; r++) {
+    if (binary[r] === '1') {
+      sum += powersOf2[r];
+    }
+  }
+  return sum;
 }
+let result = binToDec('010')
+console.log(result)
+
 
 module.exports = binToDec;
