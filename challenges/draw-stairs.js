6a809d6d339161fc7c7ca07de7433ad0bbfcc4a6
Vu Phung <vphung5707@gmail.com>
2017-09-24 10:44:00 -0700

    refactor draw-stairs code.

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -16,16 +16,27 @@
 // input: Number size
 // output: string of *
 function drawStairs(n) {
-  let symbol = '';
+  // let symbol = '';
+  //
+  // for (let i = 0, j = n; i < n; i++) {
+  //   symbol += ' '.padStart(j) + '*'.repeat(i) + '\n';
+  //   // symbol += '*'.padStart(i);
+  //   j -= 1;
+  // }
+  //
+  // return symbol;
+  if (!Number.isInteger(n) || n < 1 || n > 100 ) return;
 
-  for (let i = 0, j = n; i < n; i++) {
-    symbol += ' '.padStart(j) + '*'.repeat(i) + '\n';
-    // symbol += '*'.padStart(i);
-    j -= 1;
+  const space = ' ';
+  const start = '*';
+  var string = '';
+
+  for (var i = 1; i <= n; i += 1) {
+    string += space.repeat(n-i)+start.repeat(i)+'\n';
   }
 
-  return symbol;
+  return string;
 }
 
-// console.log(drawStairs(6));
+console.log(drawStairs(100));
 module.exports = drawStairs;
