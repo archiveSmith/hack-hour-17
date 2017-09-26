aafc3e28865ec33b9245f4872afa207ccc0c81db
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-09-26 10:00:14 -0700

    Balanced Parens Solution

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,42 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  let inputArr = input.split('');
+  const brackets = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  const letters = /^[A-Za-z]+$/;
+  const newArr = [];
+  const trashArr = [];
+  for (let x = 0; x < inputArr.length; x += 1) {
+    if (inputArr[x] === '(' || inputArr[x] === '{' || inputArr[x] === '[') {
+      newArr.push(inputArr[x]);
+    } else if (inputArr[x].match(/[a-z]/i) !== null) {
+      console.log(inputArr[x]); //
+      trashArr.push(inputArr[x]);
+    } else {
+      const last = newArr.pop();
+      if (inputArr[x] !== brackets[last]) {
+        return false;
+      }
+    }
+  }
+  if (newArr.length !== 0) {
+    return false;
+  }
+  return true;
 }
 
+
 module.exports = balancedParens;
+
+console.log(balancedParens('('));  // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
