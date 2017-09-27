65025a502f59e7b1d2db8c1f3997216b1d4ef929
Chris Kim <ckimchris@gmail.com>
2017-09-26 22:50:02 -0700

    Update balanced-parens.js

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,60 @@
  *
  */
 
-function balancedParens(input){
+var tokens = [ ['{','}'] , ['[',']'] , ['(',')'] ];
 
+
+function isParenthesis(char) {
+  var str = '{}[]()';
+  if (str.indexOf(char) > -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+function isOpenParenthesis(parenthesisChar) {
+  for (var j = 0; j < tokens.length; j++) {
+    if (tokens[j][0] === parenthesisChar) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function matches(topOfStack, closedParenthesis) {
+  for (var k = 0; k < tokens.length; k++) {
+    if (tokens[k][0] === topOfStack && 
+        tokens[k][1] === closedParenthesis) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function balancedParens(input){
+  let str = '{}[]()';
+  var expression = input.split('');
+  var stack = [];
+  for (var i = 0; i < expression.length; i++) {
+    if(str.indexOf(expression[i]) > -1){
+      if(isOpenParenthesis(expression[i])){
+        stack.push(expression[i]);
+      }else{
+        if(stack.length === 0){
+          return false
+        }else{
+          return true
+        }
+        var top = stack.pop();
+        if(!matches(top, expression[i])){
+          return false;
+        }else{
+          return true
+        }
+        }
+      }
+    }
+  }
 
 module.exports = balancedParens;
