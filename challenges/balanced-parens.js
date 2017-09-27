c6d74dd7abf37219134484aca0423bd3ea666d7c
Katherine Sampias <ksampias@gmail.com>
2017-09-26 19:35:58 -0700

    HH 9/26

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -16,6 +16,8 @@
  *  balancedParens('[({})]');   // true
  *  balancedParens('[(]{)}'); // false
  *
+ * balancedParens('[()]{}') // true ???
+ *
  * Step 3:
  * ignore non-bracket characters
  * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
@@ -25,7 +27,40 @@
  */
 
 function balancedParens(input){
+let arr = input.split('')
+  // remove all characters that are not parens, brackets, or braces
+  let filteredArr = arr.filter((letter) => {
+    return letter === '(' ||
+    letter === ')' ||
+    letter === '{' ||
+    letter === '}' ||
+    letter === '[' ||
+    letter === ']'
+  })
+  if (filteredArr.length % 2 !== 0) { return false }
+  let temp;
+  // if first index is open tag, continue / else return false
+  if (filteredArr[0] === '(' || filteredArr[0] === '[' || filteredArr[0] === '{') {
+    temp = filteredArr[0]
+  } else { return false }
+  // check if next possible index is closing tag
+  for(let i = 1; i < filteredArr.length; i += 2) {
+    if (
+      temp === '(' && filteredArr[i] === ')' ||
+      temp === '[' && filteredArr[i] === ']' ||
+      temp === '{' && filteredArr[i] === '}'
+    ) {
+      filteredArr.splice(i, 1)
+      filteredArr.shift()
+      console.log(filteredArr)
+      break
+    }
+  }
+  // once the string is empty return true
+  if (filteredArr.length === 0) {return true}
+  return (balancedParens(filteredArr.join('')))
 
 }
+
 
 module.exports = balancedParens;
