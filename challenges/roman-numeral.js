fb155e537cba083583dbea8ff2d32235f0becd20
freemanchen <freemanxchen@gmail.com>
2017-09-27 12:29:07 -0700

    Roman Numeral Fix

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -17,64 +17,24 @@
  *
  */
 
-function getCrappyRomanNumeral(n) {
-    let num = n;
-    let romans = [
-        [1,'I'],
-        [5, 'V'],
-        [10, 'X'],
-        [50, 'L'],
-        [100, 'C'],
-        [500, 'D'],
-        [1000, 'M']
-    ];
-
-    let result = '';
-    let searchForLargest;
-    searchForLargest = true;
-    for (let i = romans.length - 1; i >= 0 && searchForLargest; i--) {
-        if (num / romans[i][0] >= 1) {
-            result = romans[i][1] + result;
-            num = num - romans[i][0];
-            searchForLargest = false;
-        }
-    }
-    if (num > 0) {
-        return result + getCrappyRomanNumeral(num);
-    }
-    return result;
-}
-
-function fixRoman(string) {
-    // Not working way to fix wrong repeats
-
-
-    // let newString = string;
-    // let replacements = {
-    //     'IIII'  : 'IV',
-    //     'IIIII' : 'V',
-    //     'VV'    : 'X',
-    //     'XXXX'  : 'XL',
-    //     'XXXXX' : 'L',
-    //     'LL'    : 'C',
-    //     'CCCC'  : 'CD',
-    //     'DD'    : 'M'
-    // }
-    // for (key in replacements) {
-    //     newString = newString.replace(key, replacements[key]);
-    // }
-}
-
 function romanNumeral(n) {
     let num = parseInt(n);
     if (num !== n || num < 1) return;
     if (num === 1) return 'I';
-    let crappyRoman = getCrappyRomanNumeral(num);
-
-    // while (crappyRoman !== fixRoman(crappyRoman)) {
-    //     crappyRoman = fixRoman(crappyRoman);
-    // }
-    return crappyRoman.replace('IIII', 'IV').replace('DCCCC','CM');
+    let result = '';
+    let romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    let numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    for (let i = 0; i < numbers.length; i++) {
+        if (num >= numbers[i]) {
+            result += romans[i];
+            num = num - numbers[i];
+            break;
+        }
+    }
+    if (num > 0) result = result + romanNumeral(num);
+    return result;
 }
+
+console.log(romanNumeral(999));
 
 module.exports = romanNumeral;
