66513b9a3504692418c577cd9584c7c381532f8c
freemanchen <freemanxchen@gmail.com>
2017-09-27 10:01:34 -0700

    almost working roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,67 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
+function getCrappyRomanNumeral(n) {
+    let num = n;
+    let romans = [
+        [1,'I'],
+        [5, 'V'],
+        [10, 'X'],
+        [50, 'L'],
+        [100, 'C'],
+        [500, 'D'],
+        [1000, 'M']
+    ];
 
+    let result = '';
+    let searchForLargest;
+    searchForLargest = true;
+    for (let i = romans.length - 1; i >= 0 && searchForLargest; i--) {
+        if (num / romans[i][0] >= 1) {
+            result = romans[i][1] + result;
+            num = num - romans[i][0];
+            searchForLargest = false;
+        }
+    }
+    if (num > 0) {
+        return result + getCrappyRomanNumeral(num);
+    }
+    return result;
+}
+
+function fixRoman(string) {
+    // Not working way to fix wrong repeats
+
+
+    // let newString = string;
+    // let replacements = {
+    //     'IIII'  : 'IV',
+    //     'IIIII' : 'V',
+    //     'VV'    : 'X',
+    //     'XXXX'  : 'XL',
+    //     'XXXXX' : 'L',
+    //     'LL'    : 'C',
+    //     'CCCC'  : 'CD',
+    //     'DD'    : 'M'
+    // }
+    // for (key in replacements) {
+    //     newString = newString.replace(key, replacements[key]);
+    // }
+}
+
+function romanNumeral(n) {
+    let num = parseInt(n);
+    if (num !== n || num < 1) return;
+    if (num === 1) return 'I';
+    let crappyRoman = getCrappyRomanNumeral(num);
+
+    // while (crappyRoman !== fixRoman(crappyRoman)) {
+    //     crappyRoman = fixRoman(crappyRoman);
+    // }
+    return crappyRoman.replace('IIII', 'IV').replace('DCCCC','CM');
 }
 
 module.exports = romanNumeral;
