735624bb5fc4e6e489c87ff557b32809d760da0a
Chris Kim <ckimchris@gmail.com>
2017-09-26 22:50:56 -0700

    Update bin-to-dec.js

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,21 @@
  */
 
 function binToDec(binary) {
-
+  if(binary === 0 || binary === 1){
+    return binary;
+  }else{
+    let temp = 0;
+    let newArr = [];
+    newArr = binary.split("");
+    newArr = newArr.reverse();
+    for(let i = 0; i < newArr.length; i++){
+      newArr[i] = parseInt(newArr[i]);
+      console.log(newArr);
+      temp = Math.pow(2, i)
+      newArr[i] = newArr[i] * temp;
+    }
+    return newArr.reduce((total, num) => total + num);
+  }
 }
 
 module.exports = binToDec;
