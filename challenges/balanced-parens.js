69bc85d8ed8b6abf3c79a81dead5a4d682166368
kenhughlee <kenhughlee@yahoo.com>
2017-09-27 08:52:31 -0700

    balanced paren done

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,6 +25,56 @@
  */
 
 function balancedParens(input){
+  let parenFlag=0, curlyFlag=0, brackFlag=0;
+  let parenBegin=0, curlyBegin=0, brackBegin=0;
+  let inputArr = input.split('');
+  console.log(inputArr);
+
+  for (let i = 0; i < inputArr.length; i++) {
+
+    switch (inputArr[i]) {
+      case '(':
+        parenFlag++;
+        parenBegin = i;
+        break;
+       case ')':
+        parenFlag--;
+        if (curlyFlag!==0 || brackFlag!==0)
+          return false;
+         if (parenFlag===0)
+                balancedParens(inputArr.slice(parenBegin+1,i).join(""));
+            else return false;
+         break;
+      case '{':
+        curlyFlag++;
+        curlyBegin = i;
+        break;
+         case '}':
+         curlyFlag--;
+         if (brackFlag!==0 || parenFlag!==0)
+           return false;
+         if (curlyFlag===0)
+                balancedParens(inputArr.slice(curlyBegin+1,i).join(""));
+                  else return false;
+         break;
+        case '[':
+        brackFlag++;
+        brackBegin = i;
+        break;
+       case ']':
+          brackFlag--;
+          if (curlyFlag!==0 || parenFlag!==0)
+            return false;
+          if (brackFlag===0)
+                 balancedParens(inputArr.slice(brackBegin+1,i).join(""));
+                   else return false;
+        break;
+    }
+  }
+
+  if (parenFlag===0 && curlyFlag===0 && brackFlag===0)
+    return true;
+    else return false;
 
 }
 
