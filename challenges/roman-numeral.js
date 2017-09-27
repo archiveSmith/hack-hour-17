b90d345879bbe6827774dda3cd3619ad67289beb
brandondanh <brandondanh+github@gmail.com>
2017-09-27 09:34:16 -0700

    roman-numeral complete

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -15,10 +15,31 @@
  *      900   ->    CM
  *      1000  ->    M
  * 
+ *  XLIXVI
  */
 
 function romanNumeral(n) {
+    const num = n.split('');
+    const romNum = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+    const romNumValues = [1, 5, 10, 50, 100, 500, 1000];
+    let value = 0;
+    for(let i = 0; i < romNum.length; i++) {
+        while(num[num.length-1] === romNum[i]) {
+            value += romNumValues[i];
+            num.pop();
+            if(num.length === 0) { return value };
 
+            while(romNum.indexOf(num[num.length-1]) < i) {
+                value -= romNumValues[romNum.indexOf(num[num.length-1])];
+                num.pop();
+                if(num.length === 0) { return value };
+            }
+        }
+    }
+
+    return value;
 }
+
+console.log(romanNumeral('MDCCC'));
 
 module.exports = romanNumeral;
