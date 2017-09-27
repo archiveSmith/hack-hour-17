86c561c07d926f544db4e22af79bded6ec4c5d76
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-09-27 09:35:12 -0700

    Complete roman-numeral.js in one way

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,64 @@
  */
 
 function romanNumeral(n) {
+    let result = '';
 
+    if (n > 1000) return;
+    
+    if (n === 1000) {
+        result += 'M';
+        n -= 1000;
+    }
+    if (n >= 900) {
+        result += 'CM';
+        n -= 900;
+    }
+    if (n >= 500) {
+        result += 'D';
+        n -= 500;
+    }
+    if (n >= 400) {
+        result += 'CD';
+        n -= 400;
+    }
+    while (n >= 100) {
+      result += 'C';
+      n -= 100;
+    }
+    if (n >= 90) {
+      result += 'XC';
+      n -= 90;
+    }
+    if (n >= 50) {
+      result += 'L';
+      n -= 50;
+    }
+    if (n >= 40) {
+      result += 'XL';
+      n -= 40;
+    }
+    while (n >= 10) {
+      result += 'X';
+      n -= 10;
+    }
+    if (n >= 9) {
+      result += 'IX';
+      n -= 9;
+    }
+    if (n >= 5) {
+      result += 'V';
+      n -= 5;
+    }
+    if (n >= 4) {
+      result += 'IV';
+      n -= 4;
+    }
+    while (n >= 1) {
+      result += 'I';
+      n -= 1;
+    }
+
+  return result;
 }
 
 module.exports = romanNumeral;
