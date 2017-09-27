2a6cf1f0c9326aa9918841539c39d0d77be47f13
Katherine Sampias <ksampias@gmail.com>
2017-09-27 10:39:25 -0700

    HH 9/27

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,75 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
+  let num = n;
+  let roman = '';
 
+  if (num >= 1000) {
+    let tempCount = 0;
+    for (let i = 1000; i <= num; i += 1000) {
+      tempCount += 1000;
+      roman += 'M';
+    }
+    num -= tempCount;
+  }
+  if (num >= 900) {
+    num -= 900;
+    roman += 'CM';
+  }
+  if (num >= 500) {
+    num -= 500;
+    roman += 'D';
+  }
+  if (num === 400) {
+    num -= 400;
+    roman += 'CD';
+  }
+  if (num >= 100) {
+    let tempCount = 0;
+    for (let i = 100; i <= num; i += 100) {
+      tempCount += 100;
+      roman += 'C';
+    }
+    num -= tempCount;
+  }
+  if (num >= 50) {
+    num -= 50;
+    roman += 'L';
+  }
+  if (num === 40) {
+    num -= 40;
+    roman += 'XL';
+  }
+  if (num >= 10) {
+    let tempCount = 0;
+    for (let i = 10; i <= num; i += 10) {
+      tempCount += 10;
+      roman += 'X';
+    }
+    num-= tempCount;
+  }
+  if (num >= 5) {
+    num -= 5;
+    roman += 'V';
+  }
+  if (num === 4) {
+    num -= 4;
+    roman += 'IV';
+  }
+    if (num > 0) {
+      let tempCount = 0;
+      for (let i = 0; i < num; i += 1) {
+        tempCount += 1;
+        roman += 'I';
+      }
+    num -= tempCount;
+  }
+  console.log(num)
+  return roman;
 }
 
 module.exports = romanNumeral;
