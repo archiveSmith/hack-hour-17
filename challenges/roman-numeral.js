d78da410892575c7580d8c1f7f8b0849f8e949c9
louis <louisrouaze@gmail.com>
2017-09-27 10:48:26 -0700

    romanNumeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -19,7 +19,7 @@
 
 function romanNumeral(n) {
 	const roman =  {"M" :1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1};
-    const str = "";
+  let str = "";
 
   for (let i of Object.keys(roman) ) {
     let q = Math.floor(n / roman[i]);
