374ea2f64e42122755d6acc5ad5296e88ed580ce
brandondanh <brandondanh+github@gmail.com>
2017-09-29 09:52:22 -0700

    subset sum complete

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,22 @@
  */
 
 function subsetSum(array, target) {
+    if(target === 0)
+        return true;
+    if(array.length === 0)
+        return false;
+    for(let i = 0; i < array.length; i++) {
+        if(subsetSum(array.slice(i+1), target-array[i]))
+            return true;
+    }
 
+    return false;
 }
+
+console.log(subsetSum([3, 7, 4, 2], 5));
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32));
+console.log(subsetSum([8, 2, 4, 12], 13));
+console.log(subsetSum([8, -2, 1, -3], 6));
+
 
 module.exports = subsetSum;
