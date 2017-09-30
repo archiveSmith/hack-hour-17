ea9d9f0dee4d56ddd09f422dce35476ace5e0ff9
Katherine Sampias <ksampias@gmail.com>
2017-09-30 10:05:14 -0700

    HH 9/30

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -1,22 +1,33 @@
 /* Given two strings, write a function to check if the second one is a rotation of the first,
- * using only one call to isSubstring. The function isSubstring takes in two strings and returns
- * true if the second string is found in the first, false otherwise.
- *
- * h->e->l->l->o   ===   l->l-o->h->e
- * h->e->l->l->o   !==   o->l-l->h->e
- *
- * For example: stringRotation("hello", "hello") -> true
- *              stringRotation("hello", "llohe") -> true
- *              stringRotation("hello", "he") -> false
- *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
- */
+* using only one call to isSubstring. The function isSubstring takes in two strings and returns
+* true if the second string is found in the first, false otherwise.
+*
+* h->e->l->l->o   ===   l->l-o->h->e
+* h->e->l->l->o   !==   o->l-l->h->e
+*
+* For example: stringRotation("hello", "hello") -> true
+*              stringRotation("hello", "llohe") -> true
+*              stringRotation("hello", "he") -> false
+*              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
+*/
 
 function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
 function stringRotation(s1, s2) {
+  let startLocationInStrTwo = s2.indexOf(s1[0]);
+  if (startLocationInStrTwo === -1) {
+    return false;
+  }
+  let firstHalf = s2.slice(startLocationInStrTwo, s2.length)
+  let secondHalf = s2.slice(s2[0], startLocationInStrTwo)
+
+  let restructuredStr = firstHalf + secondHalf
+  return isSubstring(s1, restructuredStr)
 
 }
 
-module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+console.log(stringRotation("hello", "ollhe"))
+
+//module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
