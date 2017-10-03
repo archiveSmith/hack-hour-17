507e57704ba14768ae1c3245504107716353fbe5
Samantha Salley <samanthasalley@gmail.com>
2017-10-03 09:40:06 -0700

    complete match-word - working

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -7,8 +7,29 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
-function matchWord(str) {
+const isMatch = (str1, str2) => str1 === str2;
 
+const matchWord = (str) => {
+  if(!str) return true;
+  let splitStr = str.split(/[^A-Za-z]/).filter(line => line !== '');
+  let stack = [];
+  for(i = 0; i < splitStr.length; i++){
+    if(!stack.length || !isMatch(splitStr[i].toLowerCase(), stack[stack.length-1])) stack.push(splitStr[i].toLowerCase().split('').reverse().join(''));
+    else stack.pop();
+  }
+  
+  return !stack.length;
 }
+
+// ========== //
+// TEST CASES //
+// ========== //
+// console.log('true ==> ', matchWord('__END_DNE-----'));
+// console.log('false ==> ', matchWord('__ENDDNE__'));
+// console.log('true ==> ', matchWord('IF()()fi[]'));
+// console.log('false ==> ', matchWord('for__if__rof__fi'));
+// console.log('true ==> ', matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+// console.log('true ==> ', matchWord(''));
+
 
 module.exports = matchWord;
