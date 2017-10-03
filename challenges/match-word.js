d84c54bcfbeffad2bf0ded63e6e48152b27b7bcd
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-03 09:53:14 -0700

    Match Word Solution

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,30 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  const alterStr = str.toLowerCase().replace(/[\W_]+/g, ' ').split(' ');
+  const noSpaces = alterStr.filter(item => item !== '');
 
+  const stack = [];
+  for (let i = 0; i < noSpaces.length; i += 1) {
+    const reversed = noSpaces[i].split('').reverse().join('');
+
+    if (reversed === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(noSpaces[i]);
+    }
+  }
+
+  if (stack.length !== 0) return false;
+  return true;
 }
 
 module.exports = matchWord;
+
+
+console.log(matchWord('__END_DNE-----')); // -> true
+console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
+console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ] 
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')); // -> true
+console.log(matchWord('')); // -> true
