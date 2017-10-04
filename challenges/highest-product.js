6c6e9796e27a5a761f6ec8d9b1dafc5fae4e044d
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-04 09:40:51 -0700

    Highest Product Solution

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,17 @@
  */
 
 function highestProduct(array) {
-
+// base case: less than three of integers is not valid
+  if (array.length < 3) console.log('NOT VALID');
+  // reduce through array to orderfrom largest to smallest
+  const orderedArr = array.sort((a, b) => a - b);
+  const reversed = orderedArr.reverse();
+  return reversed[0] * reversed[1] * reversed[2];
+// calculate product of first three and return
 }
 
 
 module.exports = highestProduct;
+
+console.log(highestProduct([1, 2, 3, 4]));
+console.log(highestProduct([1, 3, 5, 2]));
