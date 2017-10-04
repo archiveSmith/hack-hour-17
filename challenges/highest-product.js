bbec7f96a9274ca74f66c91d076e4fcbbaf1c8d2
kenhughlee <kenhughlee@yahoo.com>
2017-10-04 09:31:39 -0700

    am i understanding the question? if i am.. done..

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
+  if (array.length<3) return;
+  let sorted = array.sort((a,b) => {
+    return b-a;
+  })
 
+  return array[0] * array[1] * array[2];
 }
 
 
 module.exports = highestProduct;
+
+console.log(highestProduct([1,6,9,3,5,7,8,4]));
