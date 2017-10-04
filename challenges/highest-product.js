71d114b45c19b6427cb230baa2c7472e1251701b
Samantha Salley <samanthasalley@gmail.com>
2017-10-04 09:39:13 -0700

    complete highest-product - working

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,17 @@
  */
 
 function highestProduct(array) {
-
+  const formattedArr = array.filter(elem => typeof elem === 'number').sort((a,b) => a-b);
+  if (formattedArr.length < 3) return;
+  const topThree = formattedArr.slice(-2);
+  return topThree.reduce((acc,val) => acc *= val, formattedArr[formattedArr.length - 3]);
 }
 
+// ========== //
+// TEST CASES //
+// ========== //
+// console.log(highestProduct([3,2,1]));
+// console.log(highestProduct([5,'a',7]));
+// console.log(highestProduct([10,5,8,29,3,15]));
 
 module.exports = highestProduct;
