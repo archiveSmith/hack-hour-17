806c1070dc105b01ceb8069279d8c108c4b18f8b
hcam05 <harry@simpleplots.com>
2017-10-04 10:00:06 -0700

    highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,18 @@
  */
 
 function highestProduct(array) {
-
+    //edge cases
+    //ignore non numeric characters
+    array.filter(x => typeof x === 'number'); //not working yet
+    if(array.length < 3) return false;
+    //sort array 
+    array.sort((a, b) => b - a);
+    //take 1st three values values
+    // console.log([array[0], array[1], array[2]]);
+    return (array[0] * array[1] * array[2]);
+    //highest value?
 }
 
+// console.log(highestProduct([1,2,3,4,5]));
 
 module.exports = highestProduct;
