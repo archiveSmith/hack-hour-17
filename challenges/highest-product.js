fe7182c069704a6fb71cecaf463708353afd3bcc
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-10-04 09:47:56 -0700

    Complete one version of highest-product.js

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,22 @@
  */
 
 function highestProduct(array) {
-
+    let arr = array.sort((a, b) => a - b);
+    
+    let lastThree = arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3];
+    let firstThree = arr[0] * arr[1] * arr [2];
+    let firstTwoAndLastOne = arr[0] * arr[1] * arr[arr.length - 1];
+    let firstOneAndLastTwo = arr[0] * arr[arr.length - 1] * arr[arr.length - 2]
+    
+    let tempArr = [lastThree, firstThree, firstTwoAndLastOne, firstOneAndLastTwo];
+    let highest = tempArr[0];
+    
+    for (let i = 0; i < 4; i++) {
+      if (tempArr[i] > highest) {
+        highest = tempArr[i];
+      }
+    }
+    return highest;
 }
 
 
