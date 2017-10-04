456a1a1481c646c8a2132015a8418eac4b0a2d0c
Chris Kim <ckimchris@gmail.com>
2017-10-03 19:55:27 -0700

    Update match-word.js

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,65 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  //find a regex to remove specials characters before and after the string
+  //find a regex to check if there is space or special character between Words
+  //set both words into the same case-insensitive
+  //the only thing that matters is the words that are at the first and last place
+  
+  if(str === ''){
+    return str;
+  }
+  
+  let test = str.replace(/\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\'|\<|\,|\.|\>|\?|\/|\""|\;|\:|\_|\s/g, ' ');
+  test = test.replace(/^\s+|\s+$/g, "");
+  
+  //str.match([+a-z*])
+  
+  let hold =[];
+  let hold2 = [];
+  let check =[];
+  
+  // for(let i = 0; i < test.length; i++){
+  //   if(test[i] === ' '){
+  //     check.push(test[i])
+  //   }else{
+  //     return false;
+  //   }
+  // }
+  
+
+  for(let i = 0; i < test.length; i++){
+    if(test[i] === ' '){
+      break;
+    }else{
+      hold.push(test[i])
+    }
+  }
+  
+  let reversed = hold.reverse();
+  console.log(reversed)
+  
+  for(let i = test.length - 1; i >= 0; i--){
+    if(test[i] === ' '){
+      break;
+    }else{
+      hold2.push(test[i])
+    }
+  }
+  
+  let reversed2 = hold2.reverse()
+  console.log(reversed2)
+  hold = reversed.join('').toLowerCase();
+  hold2 = reversed2.join('').toLowerCase();
+  
+  console.log(hold);
+  if(hold2 == hold){
+    return true
+  }else{
+    return false
+  }
 
 }
 
+console.log(matchWord('__ENDDNE__'))
 module.exports = matchWord;
