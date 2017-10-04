1b3d365f25f93f8f869dfb2f18ed5b48123acf65
Samantha Salley <samanthasalley@gmail.com>
2017-10-04 12:18:49 -0700

    complete highest-product - attempt 2

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,17 +2,26 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
-  const formattedArr = array.filter(elem => typeof elem === 'number').sort((a,b) => a-b);
-  if (formattedArr.length < 3) return;
-  const topThree = formattedArr.slice(-2);
-  return topThree.reduce((acc,val) => acc *= val, formattedArr[formattedArr.length - 3]);
+const highestProduct = (array) => {
+  if(!Array.isArray) return 0;
+  let formattedArr = array.filter(elem => typeof elem === 'number');
+  if(formattedArr.length < 3) return 0;
+  let highest = -Infinity;
+  for(let first = 0; first < formattedArr.length - 2; first++){
+    for(let sec = 1; sec < formattedArr.length - 1; sec++){
+      for(let third = 2; third < formattedArr.length; third++){
+        let prod = formattedArr[first] * formattedArr[sec] * formattedArr[third];
+        if(prod > highest) highest = prod;
+      }
+    }
+  }
+  return highest;
 }
 
 // ========== //
 // TEST CASES //
 // ========== //
-// console.log(highestProduct([3,2,1]));
+// console.log(highestProduct([-10,-1,1,2]));
 // console.log(highestProduct([5,'a',7]));
 // console.log(highestProduct([10,5,8,29,3,15]));
 
