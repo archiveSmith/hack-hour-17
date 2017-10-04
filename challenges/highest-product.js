f7ba0f10536680e61b5387d81d0a30d4ef37eb4c
brandondanh <brandondanh+github@gmail.com>
2017-10-04 09:34:52 -0700

    highest product complete

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,16 @@
  */
 
 function highestProduct(array) {
+    if (array.length < 3) {
+        return;
+    }
 
+    //check positive and negative
+    array.sort();
+    if(array[0]*array[1] > array[array.length-1]*array[array.length-2])
+        return array[0]*array[1]*array[array.length-1];
+    else
+        return array[array.length-1]*array[array.length-2]*array[array.length-3];
 }
 
 
