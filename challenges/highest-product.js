4adbb6e415342b7d186d4bbd66d3d17d030fa409
brandondanh <brandondanh+github@gmail.com>
2017-10-04 09:53:17 -0700

    highest product (I promise it works now)

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -13,14 +13,20 @@ function highestProduct(array) {
 
     //check positive and negative
     array.sort();
-    if(array[0]*array[1] > array[array.length-1]*array[array.length-2])
-        if (array[0]*array[1]*array[array.length-1] > 0)
+    if(array[0]*array[1] > array[array.length-1]*array[array.length-2]) {
+        if (array[0]*array[1]*array[array.length-1] > 0) {
             return array[0]*array[1]*array[array.length-1];
-    else
-        if (array[array.length-1]*array[array.length-2]*array[array.length-3] > 0)
+        }
+    }
+    else {
+        if (array[array.length-1]*array[array.length-2]*array[array.length-3] > 0) {
             return array[array.length-1]*array[array.length-2]*array[array.length-3];
+        }
+    }
 
     return 0;
 }
+
+console.log(highestProduct([ 1, 2, 3, 4 ]));
 
 module.exports = highestProduct;
