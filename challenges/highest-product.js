9f887b533f7936afcf88dac1f62828d03e375ad4
Pamela Lam <pamela.le.lam@gmail.com>
2017-10-04 09:54:01 -0700

    highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,23 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3) return false;
+  if (array.length === 3) return array.reduce((a, b) => a * b);
 
+  //this will be our target to be higher than
+  let highestNum = 0;
+
+  for (let i = 0; i < array.length-1; i += 1) {
+    let num = array[i];
+    let newArray = [...array.slice(0,i), ...array.slice(i+1)];
+    for (let j = 0; j < newArray.length-1; j += 1) {
+      let product = num * newArray[j] * newArray[j+1];
+      if (product > highestNum) {
+        highestNum = product;
+      }
+    }
+  }
+  return highestNum;
 }
 
 
