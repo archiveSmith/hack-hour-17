8e68afb335ded7a8a7be18209d2c47aa9676badc
Kelly <kgilliamcode@gmail.com>
2017-10-06 09:56:55 -0700

    ready for merge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,26 @@
  */
 
 function highestProduct(array) {
-
-}
+    //declare a variable which we will call 'largestProduct' Set equal to product of first three integers.
+    let largest = array[0]*array[1]*array[2];
+    let product = 1;
+    //loop through array.
+    //nest a loop through that array.
+    //nest a loop in that loop.
+    //this will check all possible combinations
+    
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i+1; j < array.length; j++) {
+        for (let k = j+1; k < array.length; k++) {
+          product = (array[i] * array[j] * array[k]);
+          if (product > largest) {
+            largest = product;
+          } 
+        }
+      }
+    }
+    return largest;
+  }
 
 
 module.exports = highestProduct;
