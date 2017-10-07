40eb5a7507223b68e2a5b9094492d974adce295c
Katherine Sampias <ksampias@gmail.com>
2017-10-07 09:57:27 -0700

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,25 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
+ const myArray = [1, 24, 4]
+
+function Stack(array) {
   // body...
+  this.push = (n) => {
+    array[array.length] = n;
+    return array.length;
+  };
+  this.pop = () => {
+    let temp = array.slice(array.length - 1, array.length);
+    array.splice(array.length - 1, 1);
+    return temp;
+  };
+  let temp;
+  for (let i = 0; i < array.length; i += 1) {
+    if (temp === undefined) { temp = array[i]; }
+    if (temp < array[i]) { temp = array[i]; }
+  }
+  return temp;
 }
 
 module.exports = Stack;
