e636d78abecdbda92f9a0dd00e0d65145cdd2c51
brandondanh <brandondanh+github@gmail.com>
2017-10-07 09:59:48 -0700

    max stack complete with getMax in O(1) time, add recursive zip linked list

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,43 @@
  */
 
 function Stack() {
-  // body...
+  this.stack = {};
+  this.maxStack = {};
+  this.length = 0;
+
+  this.push = function(element) {
+    this.stack[this.length] = element;
+    this.maxStack[this.length-1] < element || this.maxStack[this.length-1] === undefined ? 
+      this.maxStack[this.length] = element : 
+      this.maxStack[this.length] = this.maxStack[this.length-1];
+    this.length += 1;
+    return this.length;
+  },
+
+  this.pop = function() {
+    if(this.length === 0) { return;}
+    const retVal = this.stack[this.length - 1];
+    delete this.stack[this.length - 1]
+    delete this.maxStack[this.length - 1]
+    this.length -= 1;
+    return retVal; 
+  },
+
+  this.getMax = function(){
+    return this.maxStack[this.length - 1]
+
+
+    // O(n) solutions, both iterative and functional
+
+    // return Object.keys(this.stack).reduce((acc,val) => Math.max(acc,this.stack[val]), -Infinity);
+
+    // let max = -Infinity;
+    // for(let i = 0; i < this.length; i += 1){
+    //   max = Math.max(max, this.stack[i])
+    // }
+    // return max;
+  }
+
 }
 
 module.exports = Stack;
