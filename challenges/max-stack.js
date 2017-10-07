4df1d7e62f5585d1f2010cd368062a5a1ed996d0
scottrosen14 <scott.rosen14@gmail.com>
2017-10-07 09:59:54 -0700

    completed max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,30 @@
 
 function Stack() {
   // body...
+  this.body = {};
+  this.length = 0;
+  this.max = -Infinity;
+  this.push = function(item) {
+    this.body[this.length] = item;
+    this.length += 1;
+    if (item > this.max) {
+      this.max = item;
+    }
+  }
+  this.pop = function() {
+    let result = this.body[this.length-1];
+    delete this.body[this.length-1];
+    return result;
+  };
+  this.getMax = function() {
+    return this.max;
+  }
 }
-
+const stackA = new Stack();
+stackA.push(12);
+stackA.push(131);
+stackA.push(10009);
+stackA.push(92);
+console.log(stackA)
+console.log(stackA.getMax())
 module.exports = Stack;
