747a1d597891a4c1935dec46d0604253baf77308
Samantha Salley <samanthasalley@gmail.com>
2017-10-07 09:48:31 -0700

    max-stack - in progress

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,38 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
-function Stack() {
+class Stack {
   // body...
+  constructor() {
+    this.storage = {};
+    this.length = 0;
+
+    this.push.bind(this);
+    this.pop.bind(this);
+  }
+
+  push(val){
+    this.storage[this.length] = val;
+    this.length++;
+    return this.length;
+  }
+
+  pop(){
+    let removedElem = this.storage[--this.length];
+    delete this.storage[this.length];
+    return removedElem;
+  }
+
 }
+
+
+// let arr = new Stack();
+// console.log('new Stack: ', arr);
+// console.log('return of pushMe: ', arr.pushMe('Sam'));
+// console.log('after pushMe: ', arr);
+// arr.pushMe('Sivahn');
+// arr.pushMe('Steph');
+// console.log('popping steph ==> ', arr.popMe());
+// console.log('after popMe: ', arr);
 
 module.exports = Stack;
