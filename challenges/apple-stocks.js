f027f01a70dc4e6a89c6f004c0458c5265466eef
Katherine Sampias <ksampias@gmail.com>
2017-10-10 10:01:00 -0700

    apple-stocks

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,8 +12,21 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-function bestProfit(stock_prices_yesterday) {
-
+function bestProfit(stockPricesYesterday) {
+  if (!(stockPricesYesterday instanceof Array)) { return 0; }
+  let highest;
+  let lowest;
+  let profit = 0;
+  for (let i = 0; i < stockPricesYesterday.length; i += 1) {
+    if (!highest) { highest = stockPricesYesterday[i]; }
+    if (!lowest) { lowest = stockPricesYesterday[i]; }
+    if (stockPricesYesterday[i] > highest) { highest = stockPricesYesterday[i]; }
+    if (stockPricesYesterday[i] < lowest) { lowest = stockPricesYesterday[i]; }
+  }
+  if (highest > lowest) { profit = highest - lowest; }
+  // console.log('highest: ', highest, 'lowest: ', lowest, 'profit: ', profit);
+  return profit;
 }
+
 
 module.exports = bestProfit;
