c628d30d6441894f164ff4d93486508e292ea0ed
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-10 10:01:32 -0700

    Apple Stocks Solution

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,33 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
+  if (!Array.isArray(stock_prices_yesterday) || stock_prices_yesterday.length === 0) return 0;
 
+  let max = stock_prices_yesterday[0];
+  let min = stock_prices_yesterday[0];
+
+  for (let i = 0; i < stock_prices_yesterday.length; i += 1) {
+    if (stock_prices_yesterday[i] > max) {
+      max = stock_prices_yesterday[i];
+    } else if (stock_prices_yesterday[i] < min) {
+      min = stock_prices_yesterday[i];
+    }
+  }
+  if (stock_prices_yesterday.indexOf(max, -(stock_prices_yesterday.length - 1)) > stock_prices_yesterday.indexOf(min)) {
+    return max - min;
+  }
+  return 0;
 }
 
 module.exports = bestProfit;
+
+const test = [500, 100, 700, 200];
+const test1 = {};
+const test2 = [];
+const test3 = [500, 400, 300, 200];
+const test4 = [500, 300, 500, 500];
+console.log('best profit should be 600', bestProfit(test));
+console.log('not valid, should return 0', bestProfit(test1));
+console.log('not valid, should return 0', bestProfit(test2));
+console.log('no profit, should return 0', bestProfit(test3));
+console.log('best profit should be 200', bestProfit(test4));
