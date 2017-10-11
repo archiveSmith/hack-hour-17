e6695e6d0c6cede05585f8de71f0e4726be6ac16
Samantha Salley <samanthasalley@gmail.com>
2017-10-11 09:48:20 -0700

    merge-array - in progress

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,8 +13,33 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
-
+const mergeArrays = (arr1, arr2, currentPosition = 0) => {
+  console.log('running mergeArrays', arr1, arr2, currentPosition);
+  // edge cases:
+  if(!Array.isArray(arr1) || !Array.isArray(arr2)) return 0;
+  if(!arr1 || !arr1.length) return arr2;
+  if(!arr2 || !arr2.length) return arr1;
+  // main challenge:
+  if(arr1[currentPosition] < arr2[0]) {
+    currentPosition += 1;
+    arr1.splice(currentPosition,0,arr2[0]);
+  } else {
+    arr1.splice(currentPosition,0,arr2[0]);
+  }
+  arr2.shift();
+  console.log('new arr1: ', arr1);
+  console.log('new arr2: ', arr2);
+  console.log('new current position: ', currentPosition);
+  
+  return mergeArrays(arr1, arr2, currentPosition);
 }
+
+// let array1 = [1,3,5];
+// let array2 = [2,4,6];
+// console.log('mergeArrays(array1, array2): [1,2,3,4,5,6]', mergeArrays(array1, array2));
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+console.log('mergeArrays: -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21] ==> ', mergeArrays(my_array, another_array));
+
 
 module.exports = mergeArrays;
