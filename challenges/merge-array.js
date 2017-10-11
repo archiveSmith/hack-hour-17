fdcd20bb5725710b02c3702df47fc9919535383a
kenhughlee <kenhughlee@yahoo.com>
2017-10-11 09:51:57 -0700

    done

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,31 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time and O(n) space 
+ *
+ * BONUS:
+ * Complete in O(n) time and O(n) space
  *
  */
 
 function mergeArrays(arr1, arr2) {
+  let smaller, larger;
+  let index = 0;
 
+  smaller = (arr1.length<=arr2.length) ? arr1 : arr2;
+  larger = (arr2.length>= arr1.length) ? arr2 : arr1;
+
+  for (let i = 0; i < smaller.length; i++) {
+      for (j = index; smaller[i] >= larger[j]; j++) {}
+        if  (smaller[i] <= larger[j]) {
+          larger.splice(j,0,smaller[i]);
+          index = j;
+        }
+  }
+  return larger;
 }
 
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+
+console.log(mergeArrays(my_array, another_array))
 module.exports = mergeArrays;
