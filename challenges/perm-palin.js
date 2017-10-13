a2cf87f76a7ab186deffa4ab85f460990b57a32e
brandondanh <brandondanh+github@gmail.com>
2017-10-13 09:31:41 -0700

    perm palin complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,29 @@
  */
 
 function permPalin(str) {
-	
+    const strArr = str.split('');
+    const accLetters = {}
+
+    for(index in strArr) {
+        if(accLetters[strArr[index]] !== 1)
+            accLetters[strArr[index]] = 1;
+        else
+            accLetters[strArr[index]] = 0;
+    }
+
+    let unmatched = Object.keys(accLetters).reduce((acc, val) => acc + accLetters[val], 0);
+
+    if(unmatched > 1)
+        return false;
+    else
+        return true;
 }
+
+// console.log([
+// permPalin('abab'), //=> true
+// permPalin('cbaba'), //=> true
+// permPalin('cbac'), //=> false
+// permPalin('a') //=> true
+// ]);
 
 module.exports = permPalin;
