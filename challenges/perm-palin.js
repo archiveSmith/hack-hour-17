97949cfbffd6b8fd960844bd11a28db6ee6a0fb2
kenhughlee <kenhughlee@yahoo.com>
2017-10-13 09:40:05 -0700

    palin done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,23 @@
  */
 
 function permPalin(str) {
-	
+	let strArr = str.split(''),
+			checkObj = {};
+
+			strArr.forEach(element => {
+				if (checkObj[element])
+					delete checkObj[element];
+					else {
+						checkObj[element]=1;
+					}
+			})
+	let size = Object.keys(checkObj).length;
+		if (size <= 1)
+				return true;
+				else {
+					return false;
+				}
+
 }
 
 module.exports = permPalin;
