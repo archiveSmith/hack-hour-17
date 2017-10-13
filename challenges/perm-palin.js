c6c7f088c415afaf7a5be0e6bcba1858c539c2cd
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-10-13 09:49:05 -0700

    Complete perm-palin.js

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,17 @@
  */
 
 function permPalin(str) {
-	
+    if (str.length <= 1) return true;
+    
+    let oddAlready = false;
+
+    for (let i = 0; i < str.length; i += 1) {
+        let re = new RegExp(str[i], 'g');
+        let temp = str.match(re).length;
+        if (temp % 2 === 1 && oddAlready) return false;
+        if (temp % 2 === 1) oddAlready = true;
+    }
+    return true;
 }
 
 module.exports = permPalin;
