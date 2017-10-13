fde27ded03f135e945726584b179fb1704930bef
Samantha Salley <samanthasalley@gmail.com>
2017-10-13 09:19:13 -0700

    num-to-words - in progress

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,27 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+let a = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', '', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+let b = ['', 'Ten', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+let c = ['Hundred', 'Thousand', 'Million', 'Billion', 'Trillion', 'Quadrillion'];
 
+function numToWords(num) {
+  if(typeof num !== 'number') return;
+  let str = num.toString();
+  if(num === 0 && str.length === 1) return 'Zero';
+  let words = [];
+  str = str.split('').reverse().join('');
+  let len = 0;
+  let curNum = Number(str[len]);
+  while(curNum){
+    words.push(a[curNum]);
+    len++;
+    curNum = Number(str[len]);
+  }
+  return words.join('');
 }
+
+//  console.log('8 -> Eight => ', numToWords(8));
+ console.log('48 -> FortyEight => ', numToWords(8));
 
 module.exports = numToWords;
