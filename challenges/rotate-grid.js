2d19566921818669264ce0798601c43b400a708f
freemanchen <freemanxchen@gmail.com>
2017-10-14 10:00:06 -0700

    hard coded rotate grid

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -1,14 +1,14 @@
-/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents 
+/* You are given a NxN grid of elements represented by a 2D array. The ith nested array represents
  * the ith row in the grid.
- * 
- * Write a function to rotate the grid 90 degrees clockwise. 
+ *
+ * Write a function to rotate the grid 90 degrees clockwise.
  *
  * For example:     sampleGrid before:  [   [1, 2, 3],
  *                                          [4, 5, 6],
  *                                          [7, 8, 9]  ]
- *                  
+ *
  *                  rotateGrid(sampleGrid, 3);
- *                  
+ *
  *                  sampleGrid after:   [   [7, 4, 1],
  *                                          [8, 5, 2],
  *                                          [9, 6, 3]  ]
@@ -17,7 +17,27 @@
  */
 
 function rotateGrid(grid, n) {
-
+  // no idea how to make this work for any size array.  I imagine there is some way to build this destructuring dynamically...
+  [
+    grid[0][0],
+    grid[0][1],
+    grid[0][2],
+    grid[1][2],
+    grid[2][2],
+    grid[2][1],
+    grid[2][0],
+    grid[1][0]
+  ] = [
+    grid[2][0],
+    grid[1][0],
+    grid[0][0],
+    grid[0][1],
+    grid[0][2],
+    grid[1][2],
+    grid[2][2],
+    grid[2][1]
+  ]
+  return grid;
 }
 
 module.exports = rotateGrid;
