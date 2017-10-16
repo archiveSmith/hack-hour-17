b36ccbae7dc6d16ab70dc228469c0419bb151f98
brandondanh <brandondanh+github@gmail.com>
2017-10-16 09:45:51 -0700

    stack queue compelte

--- challenges/rotate-grid.js
+++ challenges/rotate-grid.js
@@ -17,7 +17,40 @@
  */
 
 function rotateGrid(grid, n) {
+    let numSquare = Math.floor(n/2);
 
+    // rotate one square at a time
+    for(let i = 0; i < numSquare ; i++) {
+        const holder = i === 0 ? grid[i].slice() : grid[i].slice(i,-i);
+        // rotate to right side
+        const size = n-i-1;
+        for(let row = i; row < holder.length + i; row++) {
+            let tmp = grid[row][size];
+            grid[row][size] = holder[row];
+            holder[row] = tmp;
+        }
+        
+        // rotate to bottom
+        console.log(holder);
+        holder.reverse();
+        for(let col = size; col >=0; col--) {
+            let tmp = grid[size][col];
+            grid[size][col] = holder[col];
+            holder[col] = tmp;
+        }
+
+        console.log(holder);
+    }
+
+    console.log(grid);
 }
+
+let sampleGrid = [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]  
+                ];  
+          
+rotateGrid(sampleGrid, 3);
 
 module.exports = rotateGrid;
--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,22 @@
 
 
 function Stack() {
+    this.stack = {};
+    this.length = 0;
 
+    this.push = (e) => {
+        this.stack[this.length] = e;
+        this.length++;
+        return e;
+    }
+
+    this.pop = () => {
+        
+        this.length--;
+        const e = this.stack[this.length];
+        delete this.stack[this.length];
+        return e;
+    }
 }
 
 
@@ -14,7 +29,44 @@ function Stack() {
 
 
 function Queue() {
+    this.leftStack = new Stack();
+    this.rightStack = new Stack();
+    this.left = true;
+    this.length = 0;
 
+    this.push = (e) => {
+            this.leftStack.push(e);       
+        this.length += 1;
+        return e;
+    }
+
+    this.pop = (e) => {
+        if(!this.rightStack.length) {
+            for(let i = 0; i < this.length; i++) {
+                this.rightStack.push(this.leftStack.pop());
+            }
+        }
+        this.length -= 1;
+        return this.rightStack.pop();
+    }
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+let queue = new Queue();
+
+console.log([
+    queue.push(1),
+    queue.push(2),
+    queue.push(3),
+    queue.push(4),
+    queue.pop(),
+    queue.pop(),
+    queue.push(5),
+    queue.push(6),
+    queue.pop(),
+    queue.push(7),
+    queue.pop(),
+    queue.pop(),
+    queue.pop()
+    ]);
+
+module.exports = { Stack: Stack, Queue: Queue };
