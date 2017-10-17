d9c0e01338c43233713a9ce00e18e2409d0c3a91
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-10-17 09:28:46 -0700

    Complete gcd.js

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
-
+  let smaller = Math.min(a, b);
+  let bigger = Math.max(a, b);
+  let gcd = 1;
+  for (let i = 2; i <= smaller; i += 1) {
+    if (smaller % i === 0 && bigger % i === 0) {
+      gcd = i;
+    }
+  }
+  return gcd;
 }
 
 module.exports = gcd;
