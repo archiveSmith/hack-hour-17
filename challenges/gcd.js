7eec55df5d3f202818e187a564de71ea13902e88
brandondanh <brandondanh+github@gmail.com>
2017-10-17 09:51:51 -0700

    gcd (faster, recursive)

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,20 +7,41 @@
  *
  */
 
+// iterative
+// function gcd(a, b) {
+//     if (!Number.isInteger(a) && !Number.isInteger(b))
+//         return undefined;
+
+//     let largest = 1;
+
+//     // less complicated, building up to the gcd
+//     for (let i = 2; i <= Math.min(a, b); i++)
+//         if (a % i === 0 && b % i === 0)
+//             largest = i;
+
+//     return largest;
+// }
+
+// recursive, using Euler's algo!
 function gcd(a, b) {
-    if (!Number.isInteger(a) && !Number.isInteger(b))
-        return undefined;
+  if (!Number.isInteger(a) && !Number.isInteger(b))
+    return undefined;
+  if( a === 0 || b === 0)
+    return 0;
+  if (a === 1 || b === 1)
+    return 1;
+  if (a === b)
+    return a;
 
-    let largest = 1;
-    // less complicated, building up to the gcd
-    for (let i = 2; i < Math.min(a, b) / i; i++)
-        if (a % i === 0 && b % i === 0)
-            largest = i;
+  const large = Math.max(a,b);
+  const small = Math.min(a,b);
+  return gcd(large-small, small);
 
-    return largest;
 }
 
-console.log(gcd(10, 8)) // -> 2
-console.log(gcd(10, 9)) //-> 1
+// console.log(gcd(10, 8)) // -> 2
+// console.log(gcd(10, 9)) //-> 1
+// console.log(gcd(252, 105))
+// console.log(gcd(24, 6))
 
 module.exports = gcd;
