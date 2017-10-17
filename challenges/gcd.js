6a7af17caf8c74a6f43bf5670d0160afd5d8c875
szmoon <szmoon@gmail.com>
2017-10-17 09:39:12 -0700

    complete gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
+  let arr = [a,b];
 
+  arr.sort(function(a,b) {
+    return a-b;
+  });
+  
+  for (let x = arr[0]; x > 0; x -=1) {
+    if (arr[1] % x === 0 && arr[0] % x === 0) {
+      return x;
+    }
+  }
 }
 
 module.exports = gcd;
+
+// console.log(gcd(50, 5));
