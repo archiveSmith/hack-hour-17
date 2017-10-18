dba1448a4d35a158357410e5ff49b45de52e1f20
brandondanh <brandondanh+github@gmail.com>
2017-10-18 09:40:31 -0700

    valid bst complete

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -13,7 +13,26 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+    if(!tree)
+        return true;
+    if(tree.left !== null && tree.left.value > tree.value)
+        return false;
+    if(tree.right !== null && tree.right.value < tree.value)
+        return false;
 
+    return (validBST(tree.left) && validBST(tree.right));
 }
+
+let a = new BinaryTree(1);
+let b = new BinaryTree(2);
+let c = new BinaryTree(3);
+let d = new BinaryTree(4);
+let e = new BinaryTree(5);
+
+c.left = a;
+a.left = b;
+c.right = d;
+d.right = e;
+console.log(validBST(c));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
