39eafe51e698eaef8cae6cb9fcf7a6a46b5b67b6
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-19 11:24:52 -0700

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,27 @@
 
 
 function Stack() {
+  this.stack = {};
+  this.size = 0;
 
+  this.add = (el) => {
+    this.size += 1;
+    this.stack[this.size] = el;
+  };
+  this.remove = () => {
+    const toDelete = this.stack[this.size];
+    delete this.stack[this.size];
+    this.size -= 1;
+    return toDelete;
+  };
 }
 
+// const test = new Stack();
+// test.add(4);
+// test.add(50);
+// console.log(test);
+// test.remove();
+// console.log(test);
 
 /**
 * Queue Class
@@ -14,7 +32,38 @@ function Stack() {
 
 
 function Queue() {
+  this.popStack = new Stack();
+  this.pushStack = new Stack();
 
+  this.enqueue = (el) => {
+    this.pushStack.add(el);
+  };
+
+  this.dequeue = () => {
+    if (this.popStack.size === 0) {
+      if (this.pushStack.size === 0) return undefined;
+      while (this.pushStack.size !== 0) {
+        this.popStack.add(this.pushStack.remove());
+      }
+    }
+    return this.popStack.remove();
+  };
 }
 
+
 module.exports = {Stack: Stack, Queue: Queue};
+
+// let testing = new Queue();
+// testing.enqueue(1);
+// console.log('added 1', testing);
+// testing.enqueue(2);
+// console.log('added 2', testing);
+// testing.enqueue(3);
+// console.log('added 3', testing);
+// testing.enqueue(5);
+// console.log('added 5', testing);
+// testing.dequeue();
+// console.log('dequeue should be 2, 3, 5', testing);
+// testing.dequeue();
+// console.log('should be 3, 5', testing);
+
