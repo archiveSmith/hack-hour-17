29230a21add2e5c2a5e75e23c18fc3388e53fc67
Vince Vuong <vuong.vincent@gmail.com>
2017-10-19 09:31:43 -0700

    Cheap solution

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,22 @@
 /*  Given an array of numbers and a target number, return true if there are two numbers in the 
  *  array that sum up to the target value; return false otherwise
+ * 
+ * ----------PSUDOCODE----------
+ * 
+ * 
  */
 
 function twoSum(arr, n) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; i < arr.length; j++) {
+      if (i+j === n) return true;
+    }
+  }
+  return false;
 }
+
+
+
+console.log(arr)
 
 module.exports = twoSum;
