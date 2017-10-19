9cf74490ee48d4d1e7d2edff260a6dd7a4970a23
Jo Zhou <jyz@MacBook-Pro-3.local>
2017-10-19 12:30:27 -0700

    rearranging files

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,19 @@
 
 
 function Stack() {
+  this.stack = {};
+  this.size = 0;
 
+  this.push = (el) => {
+    this.size += 1;
+    this.stack[this.size] = el;
+  };
+  this.pop = () => {
+    const toDelete = this.stack[this.size];
+    delete this.stack[this.size];
+    this.size -= 1;
+    return toDelete;
+  };
 }
 
 
@@ -14,7 +26,27 @@ function Stack() {
 
 
 function Queue() {
+  this.popStack = new Stack();
+  this.pushStack = new Stack();
 
+  this.enqueue = (el) => {
+    this.pushStack.push(el);
+  };
+  this.dequeue = () => {
+    if (this.popStack === 0) {
+      if (this.pushStack === 0) return undefined;
+      while (this.pushStack !== 0) {
+        this.popStack.push(this.pushStack.pop());
+      }
+    }
+    return this.popStack.pop();
+  };
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
+
+// let testing = new Queue();
+// testing.enqueue(1);
+// console.log(testing);
+// testing.enqueue(5);
+// console.log(testing);
