ac11798c99c501dc58e240d0ac30f4eae0bac9ec
Katherine Sampias <ksampias@gmail.com>
2017-10-19 10:28:11 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,19 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
 function twoSum(arr, n) {
 
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] + arr[i + 1] === n) { return true; }
+    const newArr = [arr[i], ...arr.slice(i + 2)];
+    // console.log('check:', arr[i], 'and', arr[i + 1], 'newArr:', newArr);
+    if (newArr.length < 2) { return false; }
+    if (twoSum(newArr, n)) return true;
+  }
+  return false;
 }
+
+// console.log(twoSum([4, 3, 5, 2], 8));
 
 module.exports = twoSum;
