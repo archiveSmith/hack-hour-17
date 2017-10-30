5a0fecf8af47cee41564eb575c70ab7a6718e6ab
brandondanh <brandondanh+github@gmail.com>
2017-10-30 10:53:36 -0700

    count twos complete

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -26,7 +26,24 @@
  */
 
 function applyIt(func, args) {
+  function appIt(func, args, counter) {
+    if (counter < args.length) {
+      let prevArg = args[counter];
+      console.log(prevArg);
+      return appIt( (arg, arg1, arg2) => func(prevArg, arg, arg1, arg2), args, counter + 1);
+    } else {
+      return func;
+    }
+  }
 
+  return appIt(func, args, 0);
 }
+
+
+var jae = function (name, age, location, x) {
+  return name + " is " + age + " and he lives in " + location + ' ' + x;
+};
+var jaero = applyIt(jae, ["Jae", 19, "South Carolina", 'asd']);
+console.log(jaero()); //Returns "Jae is 19 and he lives in South Carolina"
 
 module.exports = applyIt;
--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,62 @@
 
 
 function countTwos(num) {
+  // let numTwos = 0;
+  // if(num < 2 && num/10 > 0) {
+  //   return false;
+  // } else {
+  //   numTwos += 1;
+  // }
 
+  // num = Math.floor(num/10);
+
+  // for(let i = 0; num > 0; num =  Math.floor(num/10)) {
+  //   console.log(num, numTwos)
+  //   if(num%10 === 0) {
+  //     numTwos += 10 * numTwos;
+  //   } else {
+  //     numTwos += (num%10) * numTwos;
+  //   }
+  //   if(num%10 >= 2) {
+  //     numTwos += Math.pow(10,i);
+  //   }
+  //   i++;
+  // }
+
+  // return numTwos;
+
+  let originalNum = num;
+  let power = 1;
+  let i = 0;
+  let numTwos = 0;            
+
+  while (num > 0)
+  {
+      let digit = num % 10;
+      num = Math.floor(num/10);
+
+      numTwos += Math.floor(digit * (power * i) / 10);
+
+      if (digit > 2)
+          numTwos += power;
+      else if (digit == 2)
+          numTwos += originalNum % power + 1;
+
+      power *= 10;
+      i++;
+  }
+
+  return numTwos;
 }
+
+console.log(
+[
+countTwos(1),  //-> 0
+countTwos(3),  //-> 1
+countTwos(13),  //-> 2
+countTwos(20),  //-> 3
+countTwos(1000),  //-> 300
+countTwos(11420), //-> 4483
+]);
 
 module.exports = countTwos;
