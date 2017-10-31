b9c886f36c052244ebcae60fa0f84684566d9de9
Samantha Salley <samanthasalley@gmail.com>
2017-10-31 09:29:47 -0700

    add-linked-list - first attempt

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -17,8 +17,46 @@ function Node(val) {
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
+const calculateListSum = list => {
+  let sum = '';
+  let cur = list;
+  while (cur) {
+    sum += cur.value;
+    cur = cur.next;
+  }
+  return Number(sum.split('').reverse().join(''));
+};
 
+const createLinkedList = total => {
+  arr = total.toString().split('').reverse();
+  let head = new Node(Number(arr[0]));
+  let cur = head;
+  for (let i = 1; i < arr.length; i++) {
+    let newNode = new Node(Number(arr[i]));
+    cur.next = newNode;
+    cur = cur.next;
+  }
+  return head;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+function addLinkedList(l1, l2) {
+  // break down list 1
+  let sum1 = calculateListSum(l1);
+  // break down list 2
+  let sum2 = calculateListSum(l2);
+  // add numbers
+  let total = sum1 + sum2;
+  // create and return new list
+  return createLinkedList(total);
+}
+
+// let list1 = new Node(2);
+// list1.next = new Node(1);
+// list1.next.next = new Node(5);
+// let list2 = new Node(5);
+// list2.next = new Node(9);
+// list2.next.next = new Node(2);
+
+// addLinkedList(list1, list2);
+
+module.exports = { Node: Node, addLinkedList: addLinkedList };
