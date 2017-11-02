8c94151efdf4e86b6711fdf4d98ef795634ac676
Samantha Salley <samanthasalley@gmail.com>
2017-11-02 09:15:09 -0700

    repeat-number - first attempt

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,11 @@
  */
 
 function repeatNumbers(array) {
-
+  let cache = {};
+  for(let i = 0; i < array.length; i++){
+    if(cache[array[i]]) return array[i];
+    cache[array[i]] = true;
+  }
 }
 
 module.exports = repeatNumbers;
