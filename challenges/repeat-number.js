8ae39d012a14f7377be07d563aa5cdbb02f41e93
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-11-02 09:19:18 -0700

    Complete one method

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,14 @@
  */
 
 function repeatNumbers(array) {
-
+  const cache = {};
+  for (let i = 0; i < array.length; i += 1) {
+    if (cache[array[i]]) {
+      return array[i];
+    } else {
+      cache[array[i]] = true;
+    }
+  }
 }
 
 module.exports = repeatNumbers;
