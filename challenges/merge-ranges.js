93123de919db2ca874869fba3590a91e51a75c22
freemanchen <freemanxchen@gmail.com>
2017-11-03 09:28:36 -0700

    complete mergeRanges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -10,8 +10,30 @@
  */
 
 
-function mergeRanges(array) {
+// starts = [0, 3, 4. 9, 10];
+// ends = [1, 5, 8, 10, 12];
 
+function mergeRanges(array) {
+  // let starts = array.map((el) => el[0]).sort((a,b) => a - b);
+  // let ends = array.map((el) => el[1]).sort((a,b) => a - b);
+  //
+  // let output = [];
+  // for (let i = 0; i < starts.length; i += 1) {
+  //
+  // }
+  if (!Array.isArray(array)) return;
+  let arr = array.slice(0).sort((a,b) => a[0] - b[0]);
+  let output = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i + 1][0] <= arr[i][0]) {
+      output.push([arr[i][0], arr[i + 1][1]]);
+      i += 1;
+    }
+    else {
+      output.push(arr[i]);
+    }
+  }
+  return output;
 }
 
 module.exports = mergeRanges;
