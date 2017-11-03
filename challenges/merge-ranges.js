461ebd883573286d680c702e0fc9f1c96c745bc3
Jo Zhou <jyz@MacBook-Pro-4.local>
2017-11-03 09:53:56 -0700

    merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,17 @@
 
 
 function mergeRanges(array) {
-
+  array.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+  const result = [];
+  array.forEach((el) => {
+    if (!result.length || el[0] > result[result.length - 1][1]) result.push(el);
+    result[result.length - 1][1] = el[1];
+  });
+  return result;
 }
 
 module.exports = mergeRanges;
+
+
+// const times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+// mergeRanges(times); // -> [[0, 1], [3, 8], [9, 12]]
