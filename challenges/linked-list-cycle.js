ee78907118a6265fc135fc0e82dec0dd5e29e6b5
brandondanh <brandondanh+github@gmail.com>
2017-11-04 09:18:40 -0700

    comment out console.log

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -58,6 +58,6 @@ function hasCycle(head) {
 // var node5 = node4.next = new Node('5');
 // console.log(hasCycle(node1)); // => false
 // node5.next = node2;
-console.log(hasCycle(5)); // => true
+// console.log(hasCycle(5)); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
