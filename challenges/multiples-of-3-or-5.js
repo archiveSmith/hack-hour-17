c42d3881fd192c2180b2125bd7b5504530fdfa59
Samantha Salley <samanthasalley@gmail.com>
2017-11-06 09:14:48 -0800

    mult 3 or 5 - first attempt

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,6 +8,10 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
 
+  for(let i = 1; i < 1000; i++){
+    if(i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+
   return sum;
 }
 
@@ -16,6 +20,10 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
+
+  for(let i = 1; i < z; i++){
+    if(i % x === 0 || i % y === 0) sum += i;
+  }
 
   return sum;
 }
