e35aa4e349f98ea1d3511ff09d32523a8c02371e
Gordon <godronus@yahoo.com>
2017-11-09 09:26:27 -0800

    delete dups with cache

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,32 @@
 
 
 function deleteDups(head) {
-
+  const cache = {};
+  let pointer = head;
+  let lastNode = null;
+  while (pointer) {
+    if (!cache[pointer.value]) {
+      cache[pointer.value] = true;
+    } else {
+      lastNode.next = pointer.next;
+    }
+    lastNode = pointer;
+    pointer = pointer.next;
+  }
+  return head;
 }
+
+// function Node(value) {
+//   this.value = value;
+//   this.next = null;
+// }
+
+// const ll = new Node(1);
+// ll.next = new Node(2);
+// ll.next.next = new Node(2);
+// ll.next.next.next = new Node(3);
+// console.log('hi');
+
+// console.log(deleteDups(ll));
 
 module.exports = deleteDups;
