2b6ddc49191a95ee053b43de6132b9bdc928a57d
kenhughlee <kenhughlee@yahoo.com>
2017-11-09 10:07:24 -0800

    deletedups done

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,28 @@
 
 
 function deleteDups(head) {
+  let obj = {}
+  let
+  //counter=1,
+   flag=false;
 
+let theList = head;
+  obj[theList.value] = true;
+  while(theList.next && !flag) {
+    if (obj[theList.next.value]) {
+      flag = true;
+      if (theList.next.next)
+      theList.next = theList.next.next;
+      else theList.next = null;
+    }
+    else obj[theList.next.value] = true;
+    theList = theList.next;
+  }
+//console.log(head)
+  return head;
 }
 
+//var list = {value:1,next:{value:2,next:{value:3,next:{value:2,next:{value:3,next:null}}}}};
+
+//deleteDups(list)
 module.exports = deleteDups;
