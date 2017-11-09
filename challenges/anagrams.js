5a4344dc298e384cd7fa725dd9667474146bf2f6
Samantha Salley <samanthasalley@gmail.com>
2017-11-08 17:34:24 -0800

    anagrams - working

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,23 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+const anagrams = (string, start = '') => {
+  // base case
+  if (string.length === 1) {
+    return [start + string];
+  } else {
+    var returnResult = [];
+    for (var i = 0; i < string.length; i++) {
+      var result = anagrams(string.substr(0, i) + string.substr(i + 1), string[i]);
+      for (var j = 0; j < result.length; j++) {
+        returnResult.push(start + result[j]);
+      }
+    }
 
-}
+    return returnResult;
+  }
+};
+
+// console.log('6 =>', anagrams('123'));
 
 module.exports = anagrams;
