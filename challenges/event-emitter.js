a81794a79d2cbd8c31c08b111cb4bceb88e21615
Gordon <godronus@yahoo.com>
2017-11-10 09:22:27 -0800

    event-emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,40 @@
  */
 
 function EventEmitter() {
-
+  this.events = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (!this.events[funcName]) this.events[funcName] = [];
+  this.events[funcName].push(func);
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  if (this.events[funcName] ) {
+    this.events[funcName].forEach((func) => func(...args));
+  }
 };
+
+// var instance = new EventEmitter();
+// var counter = 0;
+// var counter2 = 10;
+// instance.on('increment', function(arg1, arg2) {
+//   console.log('arg1', arg1);
+//   console.log('arg2', arg2);
+//   counter++;
+// }); // counter should be 0
+// instance.on('increment', function() {
+//   counter2++;
+// });
+// console.log("counter", counter);
+// console.log("counter2", counter2);
+// instance.trigger('increment', 'hello', 'world'); // counter should be 1
+// console.log("counter", counter);
+// console.log("counter2", counter2);
+// instance.trigger('increment'); // counter should be 2
+// console.log("counter", counter);
+// console.log("counter2", counter2);
+
+
 
 module.exports = EventEmitter;
