c45e6016a21ad7544781940af726bbaa9d0cb77e
Samantha Salley <samanthasalley@gmail.com>
2017-11-11 10:40:18 -0800

    find-ordered-set - first attempt O(log n) time

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,26 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
-function findInOrderedSet(arr, target) {
-
+//  // O(log n)
+function findInOrderedSet (arr, target) {
+  if (!arr || !arr.length) return false;
+  let middle = Math.floor(arr.length / 2);
+  if (target === arr[middle]) return true;
+  if (target < arr[middle]) return findInOrderedSet(arr.slice(0, middle), target);
+  if (target > arr[middle]) return findInOrderedSet(arr.slice(middle + 1), target);
 }
 
+// // O(log n) time, O(1) space
+// function findInOrderedSet (arr, target, middle = null) {
+//   if (!arr || !arr.length) return false;
+//   if (!middle) middle = Math.floor(arr.length / 2);
+//   console.log('middle', middle);
+//   if (target === arr[middle]) return true;
+//   if (target < arr[middle]) return findInOrderedSet(arr, target, Math.floor(middle / 2));
+//   if (target > arr[middle]) return findInOrderedSet(arr, target, Math.floor(arr.length - middle / 2));
+// }
+
+// console.log('4 => true =>', findInOrderedSet([1, 4, 6, 7, 9, 17, 45], 4));
+// console.log('2 => false =>', findInOrderedSet([1, 4, 6, 7, 9, 17, 45], 2));
 
 module.exports = findInOrderedSet;
