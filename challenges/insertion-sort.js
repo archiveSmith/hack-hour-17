f26f9e2411a9c33c57efc31a1e5ed23e5f3913cd
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-11-13 09:33:25 -0800

    Complete insertion-sort.js

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let j = i - 1;
+    let temp = array[i];
 
+    while (j >= 0 && (array[j] > temp)) {
+        array[j + 1] = array[j];
+        j--;
+    }
+    array[j + 1] = temp;
+}
+return array;
 }
 
 module.exports = insertionSort;
