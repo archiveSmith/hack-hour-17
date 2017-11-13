cc6aee9f9f48e0339da10cf1986bb40e988db1b9
freemanchen <freemanxchen@gmail.com>
2017-11-13 09:23:14 -0800

    complete insertionSort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
+  if (!Array.isArray(array)) return;
+  if (array.length < 2) return array;
+  let arr = array.slice(0);
+  for (let i = 1; i < arr.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (arr[j] > arr[i]) [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  return arr;
 }
 
 module.exports = insertionSort;
