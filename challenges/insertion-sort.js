0cd57c1fd297e43a43ee6e904bfd3a629b4d1d52
woojunchoi <emilyhongji@Emily-Kim.local>
2017-11-13 09:40:44 -0800

    done

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
-}
-
+    for(let i=0; i<array.length; i++) {
+      for(let e=i+1; e<array.length; e++) {
+        if(array[i] > array[e]) {
+          let temp = array[i];
+          array[i] = array[e];
+          array[e] = temp;
+        }
+      }
+    }
+    return array;
+  }
 module.exports = insertionSort;
