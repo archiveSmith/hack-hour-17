af13193c3c7724869ad9e071d26eda14a7b36203
Pamela Lam <pamela.le.lam@gmail.com>
2017-11-14 09:25:31 -0800

    find-in-ordered-set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,11 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return true;
+  }
+  return false;
 }
 
-
 module.exports = findInOrderedSet;
+
