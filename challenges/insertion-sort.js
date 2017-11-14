93b484b22468980830c3ebfdbe772ef71f550745
Katherine Sampias <ksampias@gmail.com>
2017-11-14 09:28:46 -0800

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,21 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  const arr = array
+  for (let i = 0; i < arr.length; i += 1) {
+    // compare each index to all previous items in the array
+    for (let toSort = i + 1;
+      arr[toSort] < arr[i - 1];
+      toSort -= 1) {
+      const temp = arr[toSort - 1]
+      arr[toSort - 1] = arr[toSort]
+      arr[toSort] = temp
+    }
+  }
+  return arr
 }
 
-module.exports = insertionSort;
+// console.log(insertionSort([18, 12, 3, 9]))
+
+module.exports = insertionSort
