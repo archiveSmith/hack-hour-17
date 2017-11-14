8809af1ea14c84063d063004cf1ff70f528a78f3
Samantha Salley <samanthasalley@gmail.com>
2017-11-14 09:39:07 -0800

    unique-number - first attempt - O(n) time, O(n) space

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,20 @@
  *
  */
 function uniqueNumber(array) {
+  let act = est = 0;
+  let cache = {};
+  for (let i = 0; i < array.length; i++) {
+    if (!cache[array[i]]) {
+      est += array[i] * 2;
+      cache[array[i]] = true;
+    }
+    act += array[i];
+  }
 
+  return est - act;
 }
+
+// console.log('2 ->', uniqueNumber([1, 2, 1, 3, 3]));
+// console.log('4 ->', uniqueNumber([1, 2, 1, 2, 3, 3, 5, 4, 5]));
 
 module.exports = uniqueNumber;
