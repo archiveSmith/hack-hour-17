8d5d8bd8d6de60a1b495d9e2b749209e61580343
freemanchen <freemanxchen@gmail.com>
2017-11-14 09:20:02 -0800

    complete uniqueNumber

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,15 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
 
-}
+ function uniqueNumber(array) {
+   if (!Array.isArray(array)) return;
+   arr = array.sort((a, b) => a - b);
+   for (let i = 0; i < arr.length; i += 1) {
+     if (arr[i + 1] && arr[i + 1] === arr[i]) i += 1;
+     else return arr[i]
+   }
+   return;
+ }
 
 module.exports = uniqueNumber;
