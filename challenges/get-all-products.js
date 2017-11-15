ec264941f0430b56a198fcb0d27c5b7e2a452f83
Samantha Salley <samanthasalley@gmail.com>
2017-11-15 09:39:43 -0800

    get-all-products - first attempt - O(n^2)

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -9,8 +9,31 @@
  * do not use division, becuase zero might be in the array and you cannot divide by zero
  */
 
-function getAllProducts(array) {
+// O(n^2) time
+const getAllProducts = (array) => {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    let product;
+    for (let j = 0; j < array.length; j++) {
+      if (j !== i) {
+        if (!product) product = array[j];
+        else product *= array[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+};
 
-}
+// // O(n) time
+// const getAllProducts = (array, product) => {
+//   if (!array || !Array.isArray || array.length < 1) return;
+//   if (array.length === 1) return product !== undefined ? product : array[0];
+//   let spliced = array.splice(0, 1);
+//   product = product !== undefined ? product * spliced : spliced;
+
+// };
+
+// console.log('[84, 12, 28, 21] ->', getAllProducts([1, 7, 3, 4]));
 
 module.exports = getAllProducts;
