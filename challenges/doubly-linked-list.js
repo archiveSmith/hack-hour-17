453732cb5496c1497a7ef7733cf0e87c7e515704
Samantha Salley <samanthasalley@gmail.com>
2017-11-16 09:46:51 -0800

    doubly-linked-list - first attempt - recursive

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,78 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  let newNode = new Node(val);
+  if (!this.head) this.head = this.tail = newNode;
+  else {
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  return val;
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val, curNode = this.head) {
+  if (curNode.val === val) {
+    let prevNode = curNode.prev;
+    curNode = curNode.next;
+    if (prevNode) {
+      prevNode.next = curNode;
+    } else {
+      this.head = curNode;
+    }
+    if (curNode) {
+      curNode.prev = prevNode;
+    } else {
+      this.tail = prevNode;
+    }
+    return val;
+  } else if (curNode.next) {
+    this.remove(val, curNode.next);
+  } else {
+    return undefined;
+  }
 };
+
+// /*
+// Removes the first node with the inputted value
+//  */
+// LinkedList.prototype.remove = function (val) {
+//   let removedVal;
+//   if (this.head.val === val) {
+//     this.head = this.head.next;
+//     if (this.head) this.head.prev = null;
+//     removedVal = val;
+//   }
+//   let curNode = this.head;
+//   while (curNode) {
+//     if (curNode.val === val) {
+//       removedVal = val;
+//       let prevNode = curNode.prev;
+//       curNode = curNode.next;
+//       prevNode.next = curNode;
+//       if (curNode) {
+//         curNode.prev = prevNode;
+//       } else {
+//         this.tail = prevNode;
+//       }
+//       break;
+//     }
+//     curNode = curNode.next;
+//   }
+
+//   return removedVal;
+// };
+
+// let ll = new LinkedList();
+// ll.add(1);
+// ll.add(2);
+// ll.add(3);
+// // console.log('added 1, 2, 3', ll);
+// ll.remove(3);
+// console.log('removed 3', ll);
 
 module.exports = LinkedList;
