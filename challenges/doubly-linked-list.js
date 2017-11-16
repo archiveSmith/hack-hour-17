2387418ad2a7d853dec5e5369c34c0d8e82b8014
kenhughlee <kenhughlee@yahoo.com>
2017-11-16 09:37:25 -0800

    doubly done

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,37 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (!this.head) {
+    this.head = new Node(val);
+    this.tail = this.head;
+  }
+  else {
+    this.tail.next = new Node(val);
+    this.tail.next.prev = this.tail.next;
+    this.tail =  this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (this.head.val===val) {
+    this.head = this.head.next;
+    this.head.prev=null;
+  }
+  else {
+    while (this.head.next) {
+      if (this.head.next.val===val) {
+        if (this.head.next.next)  {
+          this.head.next = this.head.next.next;
+          this.head.next.next.prev = this.head;
+        }
+          else this.head.next = null;
+
+      }
+    }
+  }
 };
 
 module.exports = LinkedList;
