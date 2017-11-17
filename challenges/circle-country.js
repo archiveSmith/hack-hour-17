c1e7223a8215c43be5d645bb0acd47e7fa72f172
kenhughlee <kenhughlee@yahoo.com>
2017-11-17 09:36:52 -0800

    circle country done

--- challenges/circle-country.js
+++ challenges/circle-country.js
@@ -1,7 +1,8 @@
 /**
  *  Circle Country is a country that contains several circular-shaped districts.
  *  Some districts may be situated inside other districts, but their borders do not intersect or touch.
- *  Tyus is a resident of Circle Country. When he travels between two locations, he always tries to cross the fewest number of district borders as possible.
+ *  Tyus is a resident of Circle Country. When he travels between two locations,
+  he always tries to cross the fewest number of district borders as possible.
  *
  *  You are given an array x, an array y, and an array r
  *
@@ -9,10 +10,11 @@
  *
  *  In addition to the arrays, you are also given the numbers start_x, start_y, end_x, and end_y
  *
- *    - (start_x, start_y) are Tyus' starting coordinates and (end_x, end_y) are Tyus' destination coordinates
+ *  - (start_x, start_y) are Tyus' starting coordinates and (end_x, end_y) are Tyus' destination coordinates
  *
  *
- *  Write an efficient algorithm for finding the minimum number of district borders Tyus must cross in order to get from
+ *  Write an efficient algorithm for finding the minimum number of
+ district borders Tyus must cross in order to get from
  *  (start_x, start_y) to (end_x, end_y)
  *
  *  Constraints:
@@ -23,7 +25,21 @@
  */
 
 function circleCountry(x, y, r, start_x, start_y, end_x, end_y) {
-
+  // if d^2 < r^2 then inside the circle
+  // d^2 = (x1-x2)^2 + (y1-y2)^2
+  let startCount=0, endCount=0, tempDSQ;
+  // count from the starting circles
+  for (let i = 0; i<x.length; i++) {
+    tempDSQ =  ((x[i]-start_x)*(x[i]-start_x))+((y[i]-start_y)*(y[i]-start_y));
+    if (tempDSQ < (r[i]*r[i]))
+      startCount++;
+}
+  for (let i = 0; i<x.length; i++) {
+    tempDSQ =  ((x[i]-end_x)*(x[i]-end_x))+((y[i]-end_y)*(y[i]-end_y));
+    if (tempDSQ < (r[i]*r[i]))
+    endCount++;
+  }
+  return endCount+startCount;
 }
 
 module.exports = circleCountry;
