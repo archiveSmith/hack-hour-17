ec71ffde20a140bbbace8dd75b97e3db1c5fb920
freemanchen <freemanxchen@gmail.com>
2017-11-18 09:29:44 -0800

    circle country and bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  if (!Array.isArray(array)) return;
+  let arr = array.slice(0);
+  let run = true;
+  while (run) {
+      run = false
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i + 1] && arr[i] > arr[i + 1]) {
+              [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+              run = true
+          }
+      }
+  }
+  return arr;
 }
 
 module.exports = bubbleSort;
