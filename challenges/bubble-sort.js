e6fe90d52298efceeab6247789ddc4fc888ae53f
Pamela Lam <pamela.le.lam@gmail.com>
2017-11-18 09:30:27 -0800

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
+  let sorted = true;
 
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+        sorted = false;
+      }
+    }
+    if (sorted) return array;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
