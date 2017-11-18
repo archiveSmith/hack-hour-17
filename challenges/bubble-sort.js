1ecc5a704e6b8856f423e438cdee111bc41f57fa
Samantha Salley <samanthasalley@gmail.com>
2017-11-18 09:26:44 -0800

    bubble sort - first attempt

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,15 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
+const bubbleSort = (array) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) [ array[j], array[j + 1] ] = [ array[j + 1], array[j] ];
+    }
+  }
+  return array;
+};
 
-
-function bubbleSort(array) {
-
-}
+// console.log('[6, 5, 3, 1, 8, 7, 2, 4] -> [1, 2, 3, 4, 5, 6, 7, 8] ->', bubbleSort([6, 5, 3, 1, 8, 7, 2, 4]));
 
 module.exports = bubbleSort;
