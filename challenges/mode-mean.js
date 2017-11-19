76b2aa0541f9fd2154e10c584685112b711cf7e2
Pamela Lam <pamela.le.lam@gmail.com>
2017-11-19 15:38:27 -0800

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,33 +10,50 @@
  */
 
 function modemean(array) {
+  let maxFreq = 0;
+  let maxMode = -Infinity;
   let sum = 0;
-  let mode;
-  for(let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
 
-  let mean = Math.floor(sum/array.length);
-
-  let count = array.reduce(function (acc, ele) {
-    if(!acc.count[ele]) {
-      acc.count[ele] = 0;
+  array.reduce((obj, curr) => {
+    sum += curr;
+    obj[curr] ? ++obj[curr] : obj[curr] = 1;
+    if (obj[curr] === maxFreq && curr > maxMode) {
+      maxMode = curr;
+    } else if (obj[curr] > maxFreq) {
+      maxFreq = obj[curr];
+      maxMode = curr;
     }
-    acc.count[ele]++;
-    return acc;
-  }, {count: {}});
+    return obj;
+  }, {});
 
-  let values = Object.values(count.count);
-  let max = Math.max.apply(null, values);
+  return Math.floor(sum / array.length) === maxMode;
+  // let sum = 0;
+  // let mode;
+  // for(let i = 0; i < array.length; i++) {
+  //   sum += array[i];
+  // }
 
-  for(let prop in count.count) {
-    if(count.count[prop] === max) {
-      mode = prop;
-    }
-  }
+  // let mean = Math.floor(sum/array.length);
 
-  return mean = mode ? true : false;
+  // let count = array.reduce(function (acc, ele) {
+  //   if(!acc.count[ele]) {
+  //     acc.count[ele] = 0;
+  //   }
+  //   acc.count[ele]++;
+  //   return acc;
+  // }, {count: {}});
 
+  // let values = Object.values(count.count);
+  // let max = Math.max.apply(null, values);
+
+  // for(let prop in count.count) {
+  //   if(count.count[prop] === max) {
+  //     mode = prop;
+  //   }
+  // }
+
+  // return mean = mode ? true : false;
 }
 
 module.exports = modemean;
+
