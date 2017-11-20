f040a4130c4609c9824e9c6e3c6ebe3cfacd2ba4
Vu Phung <vphung5707@gmail.com>
2017-11-20 09:42:15 -0800

    Finished phonebook.

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -25,18 +25,58 @@
 */
 
 //  return the number associated with the name in the jazbook
-function findName(jazbook, name) {
-
-}
+// function findName(jazbook, name) {
+//   const contactLookUp = jazbook.filter((arrayContact) => {
+//     return arrayContact[0] === name;
+//   })
+//   return contactLookUp[0][1];
+// }
 
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
+function makePhoneBookObject(jazbook) {
+  
+  arrayToObj = {};
+  jazbook.forEach((contact) => {
+    arrayToObj[contact[0]] = contact[1];
+  });
+  return arrayToObj;
+}
 
+function findName(jazbook, name) {
+  let nameKeys = Object.keys(jazbook);
+  const nameKey = nameKeys.filter((nameKey) => {
+    return nameKey === name;
+  })
+  return jazbook[nameKey];
+}
+
+function remove(jazbook, name) {
+  let nameKeys = Object.keys(jazbook);
+  const nameKey = nameKeys.filter((nameKey) => {
+    return nameKey === name;
+  })
+  delete jazbook[nameKey];
+  return jazbook;
 }
 
 const objectToExport = {
   findName,
   makePhoneBookObject,
+  remove
 };
 
+// input you have array with sub array, name is the string you are looking for
+// use a filter function to filter the name only
+jazbook = [
+  ['alex','301-844-3421'],
+  ['jae','301-844-1211'],
+  ['david','301-844-0978'],
+  ['travis','301-844-8505'],
+  ['jasmine','1800-974-4539'],
+];
+
+// console.log(findName(jazbook, 'travis'));
+// console.log(makePhoneBookObject(jazbook));
+// console.log(findName(objectToExport.makePhoneBookObject(jazbook),'alex'));
+// console.log(remove(objectToExport.makePhoneBookObject(jazbook),'alex'));
 module.exports = objectToExport;
