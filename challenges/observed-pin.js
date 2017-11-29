bafa133da690e2cf0d5455a0a3833f7a9ca4f4ae
Gordon <godronus@yahoo.com>
2017-11-29 09:43:15 -0800

    observed pins

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -33,18 +33,46 @@ Detective, we count on you!
 
 expectations = {
   "8": ["5", "7", "8", "9", "0"],
-  "11": ["11", "22", "44", "12", "21", "14", "41", "24", "42"],
+  "11": ["11", "12", "14",    "21", "22",  "24",   "44", "41", "42"],
   "369": ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"],
 }
 
 */
 
-
-
-
 function getPINs(observed) {
+  let possArr = [];
+  const adjacents = {
+    '1': ['2', '4'],
+    '2': ['1', '5', '3'],
+    '3': ['2', '6'],
+    '4': ['1', '5', '7'],
+    '5': ['2', '4', '6', '8'],
+    '6': ['3', '5' ,'9'],
+    '7': ['4', '8'],
+    '8': ['5', '7', '9', '0'],    
+    '9': ['6', '8'],
+    '0': ['8'],
+  };
+  
+  function possibles(char, arr) {
+    const adjArr = adjacents[char].concat([char]);
+    if (arr.length === 0) return arr.concat(adjArr);
+    const result = arr.map((poss) => {
+      return adjArr.map(newComb => {
+        return newComb + poss;
+      });
+    });
+    return [].concat.apply([], result);
+  }
 
+  const numStr = observed.toString();
+  for (let i = 0, ilen = numStr.length; i < ilen; i += 1) {
+      possArr = possibles(numStr[i], possArr);
+  }
+  return possArr;
 }
+
+// console.log(getPINs(369));
 
 
 module.exports = getPINs
