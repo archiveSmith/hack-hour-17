ae7be31fc105954630690c2609ba08e360d7f0ac
Vince Vuong <vuong.vincent@gmail.com>
2017-11-29 18:45:29 -0800

    Complete

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -16,11 +16,60 @@
  * sort using helper function
  */
 
-const mergeArrays = (arr1, arr2) => {
-  return (arr1.concat(arr2)).sort(sortNumber);
-}
-
-const sortNumber = (a, b) => a - b;
-
+function mergeArrays(arr1, arr2) {
+  
+    // create a new array
+    let newArr = [];
+    // set the 2 pointers
+    let ind1 = 0;
+    let ind2 = 0;
+  
+    // check if each element in each array exists
+    // if elem in arr1 < arr2 --> push elem in arr1 then increment the index
+    // if the elem in arr1 >= arr2 --> push elem in arr2 then increment the index
+    // if one of the elems in an array doesn't exist then continue pushing on existing array elem
+    while(arr1[ind1] !== undefined || arr2[ind2] !== undefined) {
+      if(arr1[ind1] < arr2[ind2]) {
+        newArr.push(arr1[ind1++]);
+      } else if (arr1[ind1] >= arr2[ind2]) {
+        newArr.push(arr2[ind2++]);
+      } else {
+        newArr.push(arr1[ind1++]) || newArr.push(arr2[ind2++]);
+      }
+    }
+  
+    return newArr;
+  }
 
 module.exports = mergeArrays;
+
+
+
+/* -----------------------------------------------------------------
+
+SOLUTION
+function mergeArrays(arr1, arr2) {
+  
+    // create a new array
+    let newArr = [];
+    // set the 2 pointers
+    let ind1 = 0;
+    let ind2 = 0;
+  
+    // check if each element in each array exists
+    // if elem in arr1 < arr2 --> push elem in arr1 then increment the index
+    // if the elem in arr1 >= arr2 --> push elem in arr2 then increment the index
+    // if one of the elems in an array doesn't exist then continue pushing on existing array elem
+    while(arr1[ind1] !== undefined || arr2[ind2] !== undefined) {
+      if(arr1[ind1] < arr2[ind2]) {
+        newArr.push(arr1[ind1++]);
+      } else if (arr1[ind1] >= arr2[ind2]) {
+        newArr.push(arr2[ind2++]);
+      } else {
+        newArr.push(arr1[ind1++]) || newArr.push(arr2[ind2++]);
+      }
+    }
+  
+    return newArr;
+  }
+  */
