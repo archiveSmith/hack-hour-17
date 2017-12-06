6765454c41ea4ad993c0f9e4aa95de3cc1892910
brandondanh <brandondanh+github@gmail.com>
2017-12-06 09:26:54 -0800

    common element complete

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,24 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
+function commonElements (array1, array2, array3, array4) {
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  const set3 = new Set(array3);
+  const set4 = new Set(array4);
+  const count = [...set1, ...set2, ...set3, ...set4].reduce((acc, val) => {
+    acc[val] = acc[val] + 1 || 1;
+    return acc;
+  }, {});
 
+  return Object.keys(count).filter((val) => count[val] === 4);
 }
+
+// var array1 = [1, 4, 6, 7, 'ferret', 12, 12, 99, 2000, 'dog', 'dog', 99, 1000];
+// var array2 = [15, 9, 9, 'ferret', 9, 26, 12, 12, 'dog'];
+// var array3 = [23, 12, 12, 77, 'ferret', 9, 88, 100, 'dog'];
+// var array4 = ['ferret', 12, 12, 45, 9, 66, 77, 78, 2000];
+
+// console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
