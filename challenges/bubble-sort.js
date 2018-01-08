f0e99ca4d35a2df504162d1fd66e8ea9e8566783
Vincent Vuong <vuong.vincent@gmail.com>
2018-01-08 10:01:20 -0800

    Complete

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  //edge case
+  if(array.length < 2) return array;
+  //start at the end
+  for (let tail = array.length - 1; tail > 0; tail -= 1) {
+    //start at the front
+    for(let i = 0; i < tail; i+=1) {
+      if(array[i] > array[i+1]) {
+        [array[i+1], array[i]] = [array[i], array[i+1]];
+      }
+    }
+  }
+  return array;
 }
+
 
 module.exports = bubbleSort;
