de380f7517e86ec18443707065964dcdfd540ef6
Vincent Vuong <vuong.vincent@gmail.com>
2018-01-15 19:52:53 -0800

    Complete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,26 @@
 
 
 function insertionSort(array) {
+  var countOuter = 0;
+  var countInner = 0;
+  var countSwap = 0;
 
+  for(var i = 0; i < array.length; i++) {
+    countOuter++;
+    var temp = array[i];
+    var j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      countInner++;
+      countSwap++;
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
+
+  console.log('outer:', countOuter, 'inner:', countInner, 'swap:', countSwap);
+  return array;
+}
 }
 
 module.exports = insertionSort;
