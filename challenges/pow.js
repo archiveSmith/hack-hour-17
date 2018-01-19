900e72c95589c5023ad6f73303f33f6e52ad234b
Pamela Lam <pamela.le.lam@gmail.com>
2018-01-19 10:52:21 -0800

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,10 @@
  */
 
 function pow(base, power) {
-  if(power === 1) {
+  if (power === 0) {
+    return 1;
+  }
+  if (power === 1) {
     return base;
   } else {
     return base * pow(base, power-1);
@@ -11,3 +14,5 @@ function pow(base, power) {
 }
 
 module.exports = pow;
+
+console.log(pow(2,1));
