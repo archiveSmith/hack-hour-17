69a662b401f65687a6f5d4b4cd5aaf9c9370a625
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-18 23:21:06 -0800

    Func returns an array of digits

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -17,8 +17,50 @@ function Node(val) {
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
+const node1 = new Node(3);
+const node2 = new Node(5);
+const node3 = new Node(7);
+const node4 = new Node(2);
 
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+const nodeA = new Node(6);
+const nodeB = new Node(7);
+const nodeC = new Node(1);
+
+nodeA.next = nodeB;
+nodeB.next = nodeC;
+
+
+function addLinkedList(l1, l2) {
+  let curr1 = l1;
+  let curr2 = l2;
+
+  const sumArr = [];
+
+  let carryOver = 0;
+
+  while (curr1.value || curr2.value) {
+    let sum = (curr1.value === null ? 0 : curr1.value) + (curr2.value === null ? 0 : curr2.value) + carryOver;
+    carryOver = 0;
+    if (sum >= 10) {
+      sum = Number(sum.toString().slice(-1));
+      carryOver = 1;
+    }
+    sumArr.push(sum);
+    if (curr1.next === null) {
+      curr1.value = 0;
+    } else {
+      curr1 = curr1.next;
+    }
+    if (curr2.next === null) {
+      curr2.value = 0;
+    } else {
+      curr2 = curr2.next;
+    }
+  }
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
