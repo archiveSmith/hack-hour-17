4cf1f2a0842d073ccbbdcba169bc2da370611e15
Pamela Lam <pamela.le.lam@gmail.com>
2018-01-31 22:34:14 -0800

    merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,20 @@
 
 
 function mergeRanges(array) {
-
+  array.sort(function(a, b) {
+    return a[0] - b[0];
+  });
+  var result = [array[0]];
+  for (var i = 1; i < array.length; i++) {
+    var curr = array[i];
+    var last = result[result.length - 1];
+    if (curr[0] > last[1]) {
+      result.push(curr);
+    } else if (curr[1] > last[1]) {
+      last[1] = curr[1];
+    }
+  }
+  return result;
 }
 
 module.exports = mergeRanges;
