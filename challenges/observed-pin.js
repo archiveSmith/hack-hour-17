fc46bd4f20183cccabb4a1d108e05ab4685e3caa
Pamela Lam <pamela.le.lam@gmail.com>
2018-02-02 22:01:19 -0800

    observed pin

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -39,12 +39,33 @@ expectations = {
 
 */
 
-
-
-
-function getPINs(observed) {
-
+const pad = {
+  '0': ['0', '8'],
+  '1': ['1', '2', '4'],
+  '2': ['1', '2', '3', '5'],
+  '3': ['2', '3', '6'],
+  '4': ['1', '4', '5', '7'],
+  '5': ['2', '4', '5', '6', '8'],
+  '6': ['3', '5', '6', '9'],
+  '7': ['4', '7', '8'],
+  '8': ['5', '7', '8', '9', '0'],
+  '9': ['6', '8', '9']
 }
 
+function getPINs(observed) {
+  var possibilities = [''];
+  while (observed) {
+    var adjacents = pad[observed[0]];
+    var newPossibles = [];
+    for (var i = 0; i < adjacents.length; i++) {
+      for (var j = 0; j < possibilities.length; j++) {
+        newPossibles.push(possibilities[j] + adjacents[i]);
+      } 
+    }
+    possibilities = newPossibles;
+    observed = observed.slice(1);
+  }
+  return possibilities;
+}
 
 module.exports = getPINs
