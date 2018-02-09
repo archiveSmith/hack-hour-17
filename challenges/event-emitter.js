528002e0788038d489bd596b8dba92677966626c
Pamela Lam <pamela.le.lam@gmail.com>
2018-02-08 20:57:20 -0800

    event-emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -26,11 +26,19 @@ function EventEmitter() {
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (!this[funcName]) {
+    this[funcName] = [];
+  }
+  this[funcName].push(func);
 };
 
-EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+EventEmitter.prototype.trigger = function(funcName) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  if (this[funcName]) {
+    for (var i = 0; i < this[funcName].length; i++) {
+      this[funcName][i].apply(this, args);
+    }
+  }
 };
 
 module.exports = EventEmitter;
