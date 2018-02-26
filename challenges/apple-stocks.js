9071c68975593ba805377365cd4c15eb1f05263a
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-02-25 23:00:27 -0800

    Add alternative solution to apple-stocks

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -24,4 +24,25 @@ function bestProfit(stock_prices_yesterday) {
     return maxProfit;
 }
 
+function bestProfit(stock_prices_yesterday) {
+    var maxProfit = 0;
+    var possibleProfit = 0;
+    var minI = 0, maxI = 0;
+    if (stock_prices_yesterday) {
+      for (var i = 0; i < stock_prices_yesterday.length; i++) {
+        if (stock_prices_yesterday[i] < stock_prices_yesterday[minI]) {
+          minI = i;
+        }
+  
+        possibleProfit = stock_prices_yesterday[i] - stock_prices_yesterday[minI];
+  
+        if (possibleProfit > maxProfit) {
+          maxProfit = possibleProfit;
+          maxI = i;
+        }
+      }
+    }
+    return maxProfit;
+  }
+
 module.exports = bestProfit;
